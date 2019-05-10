8dd2d1064e5deb6461030c23302dbb29dcdeb31c
Daniel Molnar <dm@ikusei.de>
2019-05-10 17:50:23 +0200

    Fix tests that were missing navigation prop

--- __tests__/screens/HomeScreen.test.js
+++ __tests__/screens/HomeScreen.test.js
@@ -4,8 +4,10 @@ import renderer from 'react-test-renderer';
 import HomeScreen from '../../src/screens/HomeScreen';
 
 describe('HomeScreen', () => {
+  const navigation = { navigate: jest.fn() };
+
   it('renders correctly', () => {
-    const tree = renderer.create(<HomeScreen />).toJSON();
+    const tree = renderer.create(<HomeScreen navigation={navigation} />).toJSON();
     expect(tree).toBeTruthy();
   });
 });
