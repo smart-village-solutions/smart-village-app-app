d457dbaf167ad135109302c2e9cd5b111d90bd35
Maria Sole Cincis <m.cincis@ikusei.de>
2019-05-20 19:21:57 +0200

    Delate this folder with the aim of structuring
    - styles are now in components folder

--- src/styles/ScreenTitle.js
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components/native';
-import { colors } from '../config';
-
-export const ScreenTitle = styled.Text`
-  color: ${colors.lightestText};
-  font-weight: bold;
-  text-align: center;
-`;
