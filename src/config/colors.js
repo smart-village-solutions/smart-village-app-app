465cd5fa67181d9294d190f072e2dcf8026058ec
Maria Sole Cincis <m.cincis@ikusei.de>
2019-05-24 19:16:55 +0200

    Update colors

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,6 +1,9 @@
 export const colors = {
-  primary: '#0c5819',
-  secondary: '#09483b',
+  primary: 'rgb(12, 88, 27)',
+  secondary: 'rgb(9, 72, 59)',
+
+  // primary: 'rgb(0.05, 0.35, 0.1)',
+  // secondary: 'rgb(0.04, 0.28, 0.24)',
 
   darkText: '#222222',
   lighterText: '#bbbe64',
