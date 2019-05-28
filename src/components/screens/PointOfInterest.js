f6e28df336f54687879b98865c4e9e0cc4554a4b
Maria Sole Cincis <m.cincis@ikusei.de>
2019-05-28 19:16:22 +0200

    Create a screens folder in components
    - screens will contain all detail screens
    - by now it contains just PointOfInterest detail page
     - showing info except for url still undefined
     - TODO:  Anfahrt, Öffnungszeiten, Preise,
    - using hooks to transform the data from query for rendering

--- src/components/screens/PointOfInterest.js
+++ src/components/screens/PointOfInterest.js
@@ -1,37 +1,96 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
-import { HtmlView, Image, Link, ListSubtitle, ListTitle, Logo, Wrapper } from '../../components';
+import {
+  HtmlView,
+  Icon,
+  Image,
+  Link,
+  ListSubtitle,
+  ListTitle,
+  Title,
+  TitleContainer,
+  TitleShadow,
+  Wrapper,
+  WrapperRow
+} from '../../components';
+import { mail, location, phone as phoneIcon, url as urlIcon, arrowRight } from '../../icons';
+import { colors } from '../../config';
 
 export const PointOfInterest = ({ data }) => {
   const [page, setPage] = useState({});
 
   useEffect(() => {
-    const { name, description, category, mediaContents, dataProvider } = data;
+    const { name, description, category, mediaContents, dataProvider, addresses, contact } = data;
+    const { city, street, zip } = addresses[0];
+    const { phone, email, webUrls } = contact;
 
     setPage({
       title: name,
       body: description,
       category,
       image: mediaContents[0].sourceUrl.url,
-      logo: dataProvider.logo.url
+      address: `${street}, ${zip} ${city}`,
+      phone: `${phone}`,
+      email: `${email}`,
+      url: `${webUrls.url}`
     });
   }, []);
 
-  const { subtitle, title, body, image, link, logo } = page;
+  const { phone, email, url, title, body, image, link, address } = page;
 
   return (
     <View>
       {!!image && <Image source={{ uri: image }} />}
-      <Wrapper>
-        {!!logo && <Logo /* TODO: source={{ uri: logo}} */ />}
-        {!!subtitle && <ListSubtitle>{subtitle}</ListSubtitle>}
-        {/*TODO: map multiple contentBlocks */}
-        {!!title && <ListTitle noSubtitle>{title}</ListTitle>}
-        {!!body && <HtmlView html={body} />}
-        {!!link && <Link url={link} title={'Weiterlesen'} />}
-      </Wrapper>
+
+      <TitleShadow />
+      <TitleContainer>
+        <Title>{!!title && title.toUpperCase()}</Title>
+      </TitleContainer>
+      <TitleShadow />
+
+      <WrapperRow>
+        {!!address && <Icon icon={location(colors.primary)} />}
+        <Text>{address}</Text>
+      </WrapperRow>
+      <WrapperRow>
+        {!!phone && <Icon icon={phoneIcon(colors.primary)} />}
+        <Text>{phone}</Text>
+      </WrapperRow>
+      <WrapperRow>
+        {!!email && <Icon icon={mail(colors.primary)} />}
+        <Text>{email}</Text>
+      </WrapperRow>
+      <WrapperRow>
+        {!!url && <Icon icon={urlIcon(colors.primary)} />}
+        <Text>{url}</Text>
+      </WrapperRow>
+
+      <TitleShadow />
+      <TitleContainer>
+        <Title>{'anfahrt'.toUpperCase()}</Title>
+      </TitleContainer>
+
+      <TitleShadow />
+      <TitleShadow />
+      <TitleContainer>
+        <Title>{'öffnungszeiten'.toUpperCase()}</Title>
+      </TitleContainer>
+      <TitleShadow />
+
+      <TitleShadow />
+      <TitleContainer>
+        <Title>{'preise'.toUpperCase()}</Title>
+      </TitleContainer>
+      <TitleShadow />
+
+      <TitleShadow />
+      <TitleContainer>
+        <Title>{'beschreibung'.toUpperCase()}</Title>
+      </TitleContainer>
+      <TitleShadow />
+      <Wrapper>{!!body && <HtmlView html={body} />}</Wrapper>
     </View>
   );
 };
