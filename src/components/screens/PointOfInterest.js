73762ccf6d7fbc3de7ab1dc8f020bdf8c5a0032e
Maria Sole Cincis <m.cincis@ikusei.de>
2019-05-29 18:52:49 +0200

    Update on point of interest
    - show correctly icon+info
    - work in progress price section

--- src/components/screens/PointOfInterest.js
+++ src/components/screens/PointOfInterest.js
@@ -1,30 +1,36 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { Linking, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 import {
   HtmlView,
   Icon,
   Image,
-  Link,
-  ListSubtitle,
-  ListTitle,
   Title,
   TitleContainer,
   TitleShadow,
   Wrapper,
-  WrapperRow
+  WrapperMargin
 } from '../../components';
-import { mail, location, phone as phoneIcon, url as urlIcon, arrowRight } from '../../icons';
-import { colors } from '../../config';
-
+import { mail, location, phone as phoneIcon, url as urlIcon } from '../../icons';
+import { colors, normalize } from '../../config';
+import { PriceCard } from './PriceCard';
 export const PointOfInterest = ({ data }) => {
   const [page, setPage] = useState({});
 
   useEffect(() => {
-    const { name, description, category, mediaContents, dataProvider, addresses, contact } = data;
+    const {
+      name,
+      description,
+      category,
+      mediaContents,
+      dataProvider,
+      addresses,
+      contact,
+      webUrls
+    } = data;
     const { city, street, zip } = addresses[0];
-    const { phone, email, webUrls } = contact;
+    const { phone, email } = contact;
 
     setPage({
       title: name,
@@ -34,11 +40,11 @@ export const PointOfInterest = ({ data }) => {
       address: `${street}, ${zip} ${city}`,
       phone: `${phone}`,
       email: `${email}`,
-      url: `${webUrls.url}`
+      url: `${webUrls[0].url}`
     });
   }, []);
 
-  const { phone, email, url, title, body, image, link, address } = page;
+  const { phone, email, url, title, body, image, address } = page;
 
   return (
     <View>
@@ -49,23 +55,31 @@ export const PointOfInterest = ({ data }) => {
         <Title>{!!title && title.toUpperCase()}</Title>
       </TitleContainer>
       <TitleShadow />
+      <Wrapper>
+        <WrapperMargin>
+          {!!address && <Icon icon={location(colors.primary)} style={styles.padding} />}
+          <Text>{address}</Text>
+        </WrapperMargin>
 
-      <WrapperRow>
-        {!!address && <Icon icon={location(colors.primary)} />}
-        <Text>{address}</Text>
-      </WrapperRow>
-      <WrapperRow>
-        {!!phone && <Icon icon={phoneIcon(colors.primary)} />}
-        <Text>{phone}</Text>
-      </WrapperRow>
-      <WrapperRow>
-        {!!email && <Icon icon={mail(colors.primary)} />}
-        <Text>{email}</Text>
-      </WrapperRow>
-      <WrapperRow>
-        {!!url && <Icon icon={urlIcon(colors.primary)} />}
-        <Text>{url}</Text>
-      </WrapperRow>
+        <WrapperMargin>
+          {!!phone && <Icon icon={phoneIcon(colors.primary)} style={styles.padding} />}
+          <Text>{phone}</Text>
+        </WrapperMargin>
+
+        <WrapperMargin>
+          {!!email && <Icon icon={mail(colors.primary)} style={styles.padding} />}
+          <TouchableOpacity onPress={() => Linking.openURL(url)}>
+            <Text>{email}</Text>
+          </TouchableOpacity>
+        </WrapperMargin>
+
+        <WrapperMargin>
+          {!!url && <Icon icon={urlIcon(colors.primary)} style={styles.padding} />}
+          <TouchableOpacity onPress={() => Linking.openURL(url)}>
+            <Text>{url}</Text>
+          </TouchableOpacity>
+        </WrapperMargin>
+      </Wrapper>
 
       <TitleShadow />
       <TitleContainer>
@@ -84,6 +98,7 @@ export const PointOfInterest = ({ data }) => {
         <Title>{'preise'.toUpperCase()}</Title>
       </TitleContainer>
       <TitleShadow />
+      <PriceCard />
 
       <TitleShadow />
       <TitleContainer>
@@ -94,6 +109,11 @@ export const PointOfInterest = ({ data }) => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  padding: {
+    marginRight: 15
+  }
+});
 
 PointOfInterest.propTypes = {
   data: PropTypes.object.isRequired
