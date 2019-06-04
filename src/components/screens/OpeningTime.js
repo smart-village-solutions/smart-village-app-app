26ebe8144a0e66b7ab8034e5fe4f16688f82695f
Maria Sole Cincis <m.cincis@ikusei.de>
2019-06-04 19:52:03 +0200

    Work in progress OpeningTime
    -TODO: change the name of  the component
     - use real data from server
     - flex positioning

--- src/components/screens/OpeningTime.js
+++ src/components/screens/OpeningTime.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { device, colors, normalize } from '../../config';
 import { WrapperRow } from '../../components';
@@ -9,15 +9,42 @@ const DayTimeBox = styled.Text`
   flex: 1;
   flex-direction: column;
 `;
-const time = '10.00';
+const openingHours = [
+  {
+    weekday: 'Lorem',
+    timeFrom: 11,
+    timeTo: 18
+  },
+  {
+    weekday: 'aliqua',
+    timeFrom: 11,
+    timeTo: 18
+  },
+  {
+    weekday: 'dolore',
+    timeFrom: 11,
+    timeTo: 18
+  },
+  {
+    weekday: 'minim',
+    timeFrom: 11,
+    timeTo: 18
+  }
+];
+
 export const OpeningTime = () => (
-  <WrapperRow style={{ padding: normalize(14) }}>
-    {!!time && <DayTimeBox>Mo {time}</DayTimeBox>}
-    {!!time && <DayTimeBox>Di {time}</DayTimeBox>}
-    {!!time && <DayTimeBox>Mi {time}</DayTimeBox>}
-    {!!time && <DayTimeBox>Do {time}</DayTimeBox>}
-    {!!time && <DayTimeBox>Fr {time}</DayTimeBox>}
-    {!!time && <DayTimeBox>Sa {time}</DayTimeBox>}
-    {!!time && <DayTimeBox>So {time}</DayTimeBox>}
-  </WrapperRow>
+  <View style={{ flex: 1, flexDrection: 'row', padding: normalize(14) }}>
+    <WrapperRow>
+      {openingHours.map((item, index) => {
+        const { weekday, timeFrom, timeTo } = item;
+        return (
+          <DayTimeBox key={index}>
+            {!!weekday && <Text>{weekday}</Text>}
+            {!!timeFrom && <Text>{timeFrom}</Text>}
+            {!!timeTo && <Text>{timeTo}</Text>}
+          </DayTimeBox>
+        );
+      })}
+    </WrapperRow>
+  </View>
 );
