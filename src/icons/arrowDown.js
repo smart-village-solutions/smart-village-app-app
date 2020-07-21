35dc677df52324b76dc176397c1edf33e2bd6b40
Daniel Molnar <dm@ikusei.de>
2020-07-21 16:58:47 +0200

    refactor(icons): remove react-native-svg-uri
    - removed react-native-svg-uri in favor of react-native-svg, which
      can render from uris and xml svgs
      - needed to set defaults for width and height
    - refactored and linted files in icons/*

--- src/icons/arrowDown.js
+++ src/icons/arrowDown.js
@@ -2,7 +2,7 @@ export const arrowDown = (color) => `
   <?xml version="1.0" encoding="UTF-8"?>
   <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-    <polygon id="Fill-1"   fill="${color}" points="17.333 8 11.999 13.999 6.665 8 4 8 11.999 17 19.999 8"></polygon>
+      <polygon points="17.333 8 11.999 13.999 6.665 8 4 8 11.999 17 19.999 8" fill="${color}"></polygon>
     </g>
   </svg>
 `;
