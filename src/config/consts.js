e3de5f5d4341628bd7d0b5e69ab376a6fb07555d
Daniel Molnar <dm@ikusei.de>
2020-08-12 20:46:34 +0200

    feat(refresh interval): create logics to determine refetches of data
    - created new helper with a method to calculate and store a refresh time
      for different content types
      - the refresh time is used to make the decision for the fetch policy of
        queries smarter
      - different content types have different refresh intervals
    - created constants to reflect different refresh intervals for different types
      - currently everything has a refresh interval of once a day, which will
        change in the future for sure

--- src/config/consts.js
+++ src/config/consts.js
@@ -1,4 +1,14 @@
+const ONCE_A_DAY = 'ONCE_A_DAY';
+
 export const consts = {
   DRAWER: 'drawer',
-  TABS: 'tabs'
+  TABS: 'tabs',
+
+  // refresh intervals
+  ONCE_A_DAY: ONCE_A_DAY,
+  // refresh intervals per type:
+  STATIC_CONTENT: ONCE_A_DAY,
+  NEWS: ONCE_A_DAY,
+  EVENTS: ONCE_A_DAY,
+  POINTS_OF_INTEREST_AND_TOURS: ONCE_A_DAY
 };
---
+++ src/helpers/refreshIntervalHelper.js
@@ -0,0 +1,43 @@
+import moment from 'moment';
+import _isEmpty from 'lodash/isEmpty';
+
+import { consts } from '../config/consts';
+import { addToStore, readFromStore } from './storageHelper';
+
+/**
+ * Calculate the refresh time for a given refresh interval type.
+ * This is necessary to make comparisons while getting the fetch policy
+ * for queries.
+ * If there is already a refresh time stored, than return this, otherwise the current time.
+ * Store a new refresh time in AsyncStorage to relate to in future renderings.
+ *
+ * @param {string} refreshTimeKey the unique key of the screen to set the refresh time for
+ * @param {string} refreshInterval the refresh interval to apply
+ *
+ * @return {number} refresh time in seconds from 01.01.1970 00:00:00 UTC
+ */
+export const refreshTimeFor = async (refreshTimeKey, refreshInterval) => {
+  const refreshIntervals = await readFromStore('refresh-intervals');
+  const now = moment().unix(); // now in seconds from 01.01.1970 00:00:00 UTC
+  const refreshTime =
+    _isEmpty(refreshIntervals) || !refreshIntervals[refreshTimeKey]
+      ? now
+      : refreshIntervals[refreshTimeKey];
+
+  switch (refreshInterval) {
+  case consts.ONCE_A_DAY: {
+    const endOfDay = moment().endOf('day').unix();
+
+    if (endOfDay > refreshTime) {
+      // store or update refresh time in AsyncStorage
+      addToStore('refresh-intervals', { ...refreshIntervals, [refreshTimeKey]: endOfDay });
+    }
+
+    break;
+  }
+  default:
+    break;
+  }
+
+  return refreshTime;
+};
