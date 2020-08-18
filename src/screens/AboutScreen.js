461d00edb8c7cad5b3ad87c3288707ef8a44377f
Daniel Molnar <dm@ikusei.de>
2020-08-18 13:25:15 +0200

    feat(refresh interval): update rendering of AboutScreen
    - refactored the component to use hooks
    - added new refresh time logics to use for smarter fetch policy
      - every component, screen and content needs their own unique
        refresh time key, that will be stored in AsyncStorage and used
        to individually take care of refresh times
      - as our queries are also unique per component/screen and content
        we can build a refresh time key based on the query params

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ActivityIndicator, ScrollView } from 'react-native';
 import { Query } from 'react-apollo';
 
 import { NetworkContext } from '../NetworkProvider';
-import { colors, device, texts } from '../config';
+import { colors, consts, device, texts } from '../config';
 import {
   LoadingContainer,
   SafeAreaViewFlex,
@@ -15,53 +15,68 @@ import {
   VersionNumber
 } from '../components';
 import { getQuery } from '../queries';
-import { graphqlFetchPolicy } from '../helpers';
+import { graphqlFetchPolicy, refreshTimeFor } from '../helpers';
 
-export class AboutScreen extends React.PureComponent {
-  static contextType = NetworkContext;
+export const AboutScreen = ({ navigation }) => {
+  const [refreshTime, setRefreshTime] = useState();
+  const { isConnected, isMainserverUp } = useContext(NetworkContext);
 
-  render() {
-    const { navigation } = this.props;
-    const { isConnected, isMainserverUp } = this.context;
-    const fetchPolicy = graphqlFetchPolicy({ isConnected, isMainserverUp });
+  useEffect(() => {
+    const getRefreshTime = async () => {
+      const time = await refreshTimeFor('publicJsonFile-homeAbout', consts.STATIC_CONTENT);
 
+      setRefreshTime(time);
+    };
+
+    getRefreshTime();
+  }, []);
+
+  if (!refreshTime) {
     return (
-      <SafeAreaViewFlex>
-        <Query
-          query={getQuery('publicJsonFile')}
-          variables={{ name: 'homeAbout' }}
-          fetchPolicy={fetchPolicy}
-        >
-          {({ data, loading }) => {
-            if (loading) {
-              return (
-                <LoadingContainer>
-                  <ActivityIndicator color={colors.accent} />
-                </LoadingContainer>
-              );
-            }
-
-            let publicJsonFileContent =
-              data && data.publicJsonFile && JSON.parse(data.publicJsonFile.content);
-
-            if (!publicJsonFileContent || !publicJsonFileContent.length) return <VersionNumber />;
-
-            return (
-              <ScrollView>
-                <TitleContainer>
-                  <Title>{texts.homeTitles.about}</Title>
-                </TitleContainer>
-                {device.platform === 'ios' && <TitleShadow />}
-                <TextList navigation={navigation} data={publicJsonFileContent} noSubtitle />
-                <VersionNumber />
-              </ScrollView>
-            );
-          }}
-        </Query>
-      </SafeAreaViewFlex>
+      <LoadingContainer>
+        <ActivityIndicator color={colors.accent} />
+      </LoadingContainer>
     );
   }
-}
+
+  const fetchPolicy = graphqlFetchPolicy({ isConnected, isMainserverUp, refreshTime });
+
+  return (
+    <SafeAreaViewFlex>
+      <Query
+        query={getQuery('publicJsonFile')}
+        variables={{ name: 'homeAbout' }}
+        fetchPolicy={fetchPolicy}
+      >
+        {({ data, loading }) => {
+          if (loading) {
+            return (
+              <LoadingContainer>
+                <ActivityIndicator color={colors.accent} />
+              </LoadingContainer>
+            );
+          }
+
+          let publicJsonFileContent =
+            data && data.publicJsonFile && JSON.parse(data.publicJsonFile.content);
+
+          if (!publicJsonFileContent || !publicJsonFileContent.length) return <VersionNumber />;
+
+          return (
+            <ScrollView>
+              <TitleContainer>
+                <Title>{texts.homeTitles.about}</Title>
+              </TitleContainer>
+              {device.platform === 'ios' && <TitleShadow />}
+              <TextList navigation={navigation} data={publicJsonFileContent} noSubtitle />
+              <VersionNumber />
+            </ScrollView>
+          );
+        }}
+      </Query>
+    </SafeAreaViewFlex>
+  );
+};
 
 AboutScreen.propTypes = {
   navigation: PropTypes.object.isRequired
