f193d2195175144df28c0236e1b8089f01487b0c
Daniel Molnar <dm@ikusei.de>
2020-09-24 15:01:18 +0200

    feat(orientation change): add orientation info to orientation context
    - added `orientation` (`'portrait'` or `'landscape'`)
    - update orientation on device orientation change
    - added a timeout to wait a short period of time before
      reading and setting the orientation and dimensions, because
      some devices may take longer then other to update the orientation.
    
    SVA-93

--- src/OrientationProvider.js
+++ src/OrientationProvider.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { createContext, useEffect, useState } from 'react';
 import { Dimensions } from 'react-native';
-
 import * as ScreenOrientation from 'expo-screen-orientation';
 
 const defaultDimensions = {
@@ -9,20 +8,43 @@ const defaultDimensions = {
   width: Dimensions.get('window').width
 };
 
+// default orientation should be portrait up, because we defined that in the expo configs. but we
+// want to be sure with checking the devive dimension.
+const defaultOrientation =
+  defaultDimensions.width < defaultDimensions.height ? 'portrait' : 'landscape';
+
 export const OrientationContext = createContext({
+  orientation: defaultOrientation,
   dimensions: defaultDimensions
 });
 
+const getOrientation = (orientation) => {
+  if (
+    orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT ||
+    orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT
+  ) {
+    return 'landscape';
+  }
+
+  return 'portrait';
+};
+
 export const OrientationProvider = ({ children }) => {
   const [dimensions, setDimensions] = useState(defaultDimensions);
+  const [orientation, setOrientation] = useState(defaultOrientation);
 
   useEffect(() => {
-    ScreenOrientation.addOrientationChangeListener(() => {
-      // https://docs.expo.io/versions/latest/sdk/screen-orientation/#screenorientationaddorientationchangelistenerlistener
-      setDimensions({
-        height: Dimensions.get('window').height,
-        width: Dimensions.get('window').width
-      });
+    ScreenOrientation.addOrientationChangeListener(({ orientationInfo }) => {
+      // we need to wait a short period of time before reading and setting the orientation and
+      // dimensions, because some devices may take longer then other to update the orientation.
+      setTimeout(() => {
+        // https://docs.expo.io/versions/latest/sdk/screen-orientation/#screenorientationaddorientationchangelistenerlistener
+        setOrientation(getOrientation(orientationInfo.orientation));
+        setDimensions({
+          height: Dimensions.get('window').height,
+          width: Dimensions.get('window').width
+        });
+      }, 300);
     });
 
     // returned function will be called on component unmount
@@ -31,7 +53,7 @@ export const OrientationProvider = ({ children }) => {
   }, []);
 
   return (
-    <OrientationContext.Provider value={{ dimensions }}>
+    <OrientationContext.Provider value={{ orientation, dimensions }}>
       {children}
     </OrientationContext.Provider>
   );
