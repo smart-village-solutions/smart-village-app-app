f0f468ad170235d216b0ac74020721c8c46e0974
Daniel Molnar <dm@ikusei.de>
2020-10-01 17:36:17 +0200

    refactor: changed switch statements to objects
    - DeepCode was arguing with duplications in the case body (return)
      and suggested to use fall-through
    - fall-throughs in cases are not that readable, so i decided to go with objects instead
      - thanks for the idea to https://dev.to/arisa_dev/javascript-is-fallthrough-from-switch-statement-a-troublemaker-39o

--- src/queries/index.js
+++ src/queries/index.js
@@ -15,45 +15,34 @@ import { QUERY_TYPES } from './types';
 
 export * from './types';
 
-/* eslint-disable complexity */
-/* NOTE: we need to check a lot for presence, so this is that complex */
 export const getQuery = (query, filterOptions = {}) => {
-  switch (query) {
-  case QUERY_TYPES.CATEGORIES:
-    return GET_CATEGORIES;
-  case QUERY_TYPES.EVENT_RECORD:
-    return GET_EVENT_RECORD;
-  case QUERY_TYPES.EVENT_RECORDS:
-    return GET_EVENT_RECORDS;
-  case QUERY_TYPES.NEWS_ITEM:
-    return GET_NEWS_ITEM;
-  case QUERY_TYPES.NEWS_ITEMS:
-    return filterOptions.showNewsFilter
+  const QUERIES = {
+    [QUERY_TYPES.CATEGORIES]: GET_CATEGORIES,
+    [QUERY_TYPES.EVENT_RECORD]: GET_EVENT_RECORD,
+    [QUERY_TYPES.EVENT_RECORDS]: GET_EVENT_RECORDS,
+    [QUERY_TYPES.NEWS_ITEM]: GET_NEWS_ITEM,
+    [QUERY_TYPES.NEWS_ITEMS]: filterOptions.showNewsFilter
       ? GET_FILTERED_NEWS_ITEMS_AND_DATA_PROVIDERS
-      : GET_NEWS_ITEMS;
-  case QUERY_TYPES.TOUR:
-    return GET_TOUR;
-  case QUERY_TYPES.TOURS:
-    return GET_TOURS;
-  case QUERY_TYPES.POINT_OF_INTEREST:
-    return GET_POINT_OF_INTEREST;
-  case QUERY_TYPES.POINTS_OF_INTEREST:
-    return GET_POINTS_OF_INTEREST;
-  case QUERY_TYPES.POINTS_OF_INTEREST_AND_TOURS:
-    return GET_POINTS_OF_INTEREST_AND_TOURS;
-  case QUERY_TYPES.PUBLIC_HTML_FILE:
-    return GET_PUBLIC_HTML_FILE;
-  case QUERY_TYPES.PUBLIC_JSON_FILE:
-    return GET_PUBLIC_JSON_FILE;
-  }
+      : GET_NEWS_ITEMS,
+    [QUERY_TYPES.TOUR]: GET_TOUR,
+    [QUERY_TYPES.TOURS]: GET_TOURS,
+    [QUERY_TYPES.POINT_OF_INTEREST]: GET_POINT_OF_INTEREST,
+    [QUERY_TYPES.POINTS_OF_INTEREST]: GET_POINTS_OF_INTEREST,
+    [QUERY_TYPES.POINTS_OF_INTEREST_AND_TOURS]: GET_POINTS_OF_INTEREST_AND_TOURS,
+    [QUERY_TYPES.PUBLIC_HTML_FILE]: GET_PUBLIC_HTML_FILE,
+    [QUERY_TYPES.PUBLIC_JSON_FILE]: GET_PUBLIC_JSON_FILE
+  };
+
+  return QUERIES[query];
 };
-/* eslint-enable complexity */
 
 export const getFetchMoreQuery = (query, filterOptions = {}) => {
-  switch (query) {
-  case QUERY_TYPES.EVENT_RECORDS:
-    return GET_EVENT_RECORDS;
-  case QUERY_TYPES.NEWS_ITEMS:
-    return filterOptions.showNewsFilter ? GET_FILTERED_NEWS_ITEMS : GET_NEWS_ITEMS;
-  }
+  const FETCH_MORE_QUERIES = {
+    [QUERY_TYPES.EVENT_RECORDS]: GET_EVENT_RECORDS,
+    [QUERY_TYPES.NEWS_ITEMS]: filterOptions.showNewsFilter
+      ? GET_FILTERED_NEWS_ITEMS
+      : GET_NEWS_ITEMS
+  };
+
+  return FETCH_MORE_QUERIES[query];
 };
