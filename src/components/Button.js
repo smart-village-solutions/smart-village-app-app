354f791a476db2c3aa53c83cb1d0882c68fe06b0
Maria Sole Cincis <msc@tpwd.de>
2020-10-02 18:13:46 +0200

    feat(button): new style for button on landscape mode
    - implemented OrientationContext , which knows when device is portrait or landscape mode
    - buttonStyle condition , when on landscape mode button is on 60% of its with and centred in the screen
      - if portrait we don't want to render new style for button so it must be an empty object
    - made tests with button on 50% , screen shots will follow on PR
    
    SVA-93

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button as RNEButton } from 'react-native-elements';
 
 import { colors, normalize } from '../config';
 import { DiagonalGradient } from './DiagonalGradient';
+import { OrientationContext } from '../OrientationProvider';
 
 export const Button = ({ title, onPress }) => {
+  const { orientation } = useContext(OrientationContext);
   return (
     <RNEButton
       onPress={onPress}
@@ -14,6 +16,7 @@ export const Button = ({ title, onPress }) => {
       titleStyle={styles.titleStyle}
       containerStyle={styles.containerStyle}
       ViewComponent={DiagonalGradient}
+      buttonStyle={orientation === 'landscape' ? styles.buttonStyle : {}}
       useForeground
     />
   );
@@ -23,6 +26,10 @@ const styles = StyleSheet.create({
   titleStyle: {
     color: colors.lightestText,
     fontFamily: 'titillium-web-bold'
+  },
+  buttonStyle: {
+    width: '60%',
+    alignSelf: 'center'
   },
   containerStyle: {
     marginBottom: normalize(21)
