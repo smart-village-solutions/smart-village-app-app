a7a41bf298b90d240b145f6587026f6428f6616e
Daniel Molnar <dm@ikusei.de>
2020-10-22 12:52:19 +0200

    refactor: add JSDoc @return for functions

--- src/components/TabBarIcon.js
+++ src/components/TabBarIcon.js
@@ -8,7 +8,9 @@ import { Icon } from './Icon';
 
 /**
  * Smart icon component for the tab bar, which adds logics to styling based on orientation and
- * platform. It is a wrapper and renders the main Icon component.
+ * platform. It is a wrapper and renders the main `Icon` component.
+ *
+ * @return Icon - main `Icon` component
  */
 export const TabBarIcon = ({ xml, width, height, name, size, focused, style, landscapeStyle }) => {
   const { orientation } = useContext(OrientationContext);
