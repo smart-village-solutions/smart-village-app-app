3346b8fe7e8c600fff0ad5a9fa949850f65b888a
Daniel Molnar <dm@tpwd.de>
2020-11-19 19:54:47 +0100

    feat(settings): add Switch
    - created own `Switch` based on the one from React Native
      to be able to style and use it as we want
      - https://reactnative.dev/docs/switch
    
    SVA-91

---
+++ src/components/Switch.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Platform, Switch as RNSwitch } from 'react-native';
+
+import { colors } from '../config';
+
+const trackColor = {
+  ...Platform.select({
+    android: { false: colors.shadow, true: colors.lighterPrimary },
+    ios: { false: colors.shadow, true: colors.primary }
+  })
+};
+const thumbColor = colors.lightestText;
+const thumbColorEnabled = Platform.select({
+  android: colors.primary,
+  ios: colors.lightestText
+});
+
+export const Switch = ({ switchValue, setSwitchValue }) => (
+  <RNSwitch
+    trackColor={trackColor}
+    thumbColor={switchValue ? thumbColorEnabled : thumbColor}
+    ios_backgroundColor={colors.shadow}
+    onValueChange={setSwitchValue}
+    value={switchValue}
+  />
+);
+
+Switch.propTypes = {
+  switchValue: PropTypes.bool.isRequired,
+  setSwitchValue: PropTypes.func.isRequired
+};
