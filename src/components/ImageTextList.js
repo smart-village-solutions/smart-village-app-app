99446c3274b8fb85407f22cc4f4b9d010618db2a
Daniel Molnar <dm@tpwd.de>
2020-11-26 14:59:56 +0100

    fix(ImageTextList): add missing props
    - added passing of all props, as there were some props missing,
      which the `TextList` needs
    
    SVA-61

--- src/components/ImageTextList.js
+++ src/components/ImageTextList.js
@@ -1,13 +1,5 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
 import { TextList } from './TextList';
 
-export const ImageTextList = ({ navigation, data }) => (
-  <TextList navigation={navigation} data={data} leftImage />
-);
-
-ImageTextList.propTypes = {
-  navigation: PropTypes.object.isRequired,
-  data: PropTypes.array.isRequired
-};
+export const ImageTextList = (props) => <TextList {...props} leftImage />;
