8e4977015a7e291a01c41326667346eedfb06dbb
Julian Kwast <jk@tpwd.de>
2020-12-04 11:05:47 +0100

    fix(typescript): Touchable
    - fixed: JSX element does not have any construct or call signatures.
    
    SVA-86

--- src/components/Touchable.js
+++ src/components/Touchable.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { TouchableNativeFeedback, TouchableOpacity } from 'react-native';
 
 import { device } from '../config';
 
-export const Touchable = device.platform === 'ios' ? TouchableOpacity : TouchableNativeFeedback;
+export const Touchable = (props) => device.platform === 'ios' ? <TouchableOpacity {...props} /> : <TouchableNativeFeedback {...props} />;
