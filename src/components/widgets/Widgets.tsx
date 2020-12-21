42ae831477951763899945f68cd9b505ffb24ef0
Julian Kwast <jk@tpwd.de>
2020-12-21 15:41:11 +0100

    feature(widgets): setup configuration
    - added widgets component to homescreen
    - added destructuring from global settings for widgets

---
+++ src/components/widgets/Widgets.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
+
+type Props = {
+  navigation: NavigationScreenProp<any>;
+  widgets?: string[];
+}
+
+const EXISTING_WIDGETS: {[key: string]: React.FC<{navigation: NavigationScreenProp<any>}>} = {};
+
+const getExistingWidgets = (widgets: Array<string>) => {
+  const existingWidgets = widgets.map((widget) => EXISTING_WIDGETS[widget]);
+  return existingWidgets.filter((item) => item !== undefined);
+};
+
+export const Widgets = ({ navigation, widgets }: Props) => {
+  if(!widgets) return null;
+
+  const filteredWidgets = getExistingWidgets(widgets);
+
+  if (!filteredWidgets?.length) return null;
+
+  const widgetComponents = filteredWidgets.map(
+    (Component, index) => {
+      return <Component key={index} navigation={navigation} />;
+    }
+  );
+
+  return (
+    <View style={styles.container}>
+      {widgetComponents}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
