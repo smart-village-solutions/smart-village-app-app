c62abbcc1341c5c03e60b26f9e8ed113f52e03f2
Julian Kwast <jk@tpwd.de>
2020-12-21 16:50:12 +0100

    feature(event widget): added event widget
    - extracted ListHeaderComponent from ListComponent
      - this fixes layout issues for empty lists
    - changed condition to in index screen to still render header and list if data length is 0
      - otherwise the filter logic needs to be entirely reworked for the event widget
    - added new files for the widget
    - added new calendar icon
    
    SVA-82

---
+++ src/components/widgets/EventWidget.tsx
@@ -0,0 +1,78 @@
+import moment from 'moment';
+import React, { useCallback, useContext } from 'react';
+import { useQuery } from 'react-apollo';
+import { StyleSheet, View } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
+
+import { colors, consts, normalize, texts } from '../../config';
+import { graphqlFetchPolicy } from '../../helpers';
+import { useRefreshTime } from '../../hooks';
+import { calendar } from '../../icons';
+import { NetworkContext } from '../../NetworkProvider';
+import { getQuery, QUERY_TYPES } from '../../queries';
+import { Icon } from '../Icon';
+import { BoldText, RegularText } from '../Text';
+import { Touchable } from '../Touchable';
+import { Wrapper } from '../Wrapper';
+
+type Props = {
+  navigation: NavigationScreenProp<never>;
+};
+
+export const EventWidget = ({ navigation }: Props) => {
+  const refreshTime = useRefreshTime('event-widget', consts.REFRESH_INTERVALS.ONCE_A_DAY);
+  const { isConnected, isMainserverUp } = useContext(NetworkContext);
+
+  const fetchPolicy = graphqlFetchPolicy({ isConnected, isMainserverUp, refreshTime });
+
+  const currentDate = moment().format('YYYY-MM-DD');
+  const queryVariables = {
+    dateRange: [currentDate, currentDate]
+  };
+
+  const { data, loading } = useQuery(getQuery(QUERY_TYPES.EVENT_RECORDS), {
+    fetchPolicy,
+    variables: queryVariables
+  });
+
+  const onPress = useCallback(() => {
+    navigation.navigate('Index', {
+      title: 'Veranstaltungen',
+      query: QUERY_TYPES.EVENT_RECORDS,
+      queryVariables,
+      rootRouteName: 'EventRecords'
+    });
+  }, [navigation]);
+
+  const eventCount = data?.eventRecords?.length;
+
+  return (
+    <Touchable onPress={onPress}>
+      <Wrapper>
+        <View style={styles.container}>
+          <Icon style={styles.icon} xml={calendar(colors.primary)} />
+          <BoldText style={styles.count}>{loading ? ' ' : eventCount}</BoldText>
+        </View>
+        <RegularText style={styles.text}>{texts.homeTitles.events}</RegularText>
+      </Wrapper>
+    </Touchable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  count: {
+    color: colors.primary,
+    fontSize: normalize(20),
+    paddingTop: normalize(4)
+  },
+  icon: {
+    paddingHorizontal: normalize(8)
+  },
+  text: {
+    color: colors.primary
+  }
+});
--- src/icons/index.js
+++ src/icons/index.js
@@ -2,6 +2,7 @@ export * from './arrowDown';
 export * from './arrowLeft';
 export * from './arrowRight';
 export * from './arrowUp';
+export * from './calendar';
 export * from './drawerMenu';
 export * from './favSettings';
 export * from './heartEmpty';
