bb84c91fc42e49b57449c6483b037c9c3802d386
Julian Kwast <jk@tpwd.de>
2021-01-11 11:34:32 +0100

    fix(construction site widget): screen name
    - changed the screen name of the detail page to be singular
    
    SVA-83

--- src/navigation/AppStackNavigator.js
+++ src/navigation/AppStackNavigator.js
@@ -46,7 +46,7 @@ const AppStackNavigator = (headerRight = true) =>
       ConstructionSiteOverview: {
         screen: ConstructionSiteOverviewScreen,
         navigationOptions: () => ({
-          title: texts.screenTitles.constructionSite
+          title: texts.screenTitles.constructionSites
         })
       },
       Index: {
