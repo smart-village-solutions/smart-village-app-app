59291d7a75712a251686ed5d556442f0c16e8172
Daniel Molnar <dm@tpwd.de>
2021-01-19 15:40:17 +0100

    feat(image aspect ratio): set IMAGE_ASPECT_RATIO with value from global settings
    - added setter for `IMAGE_ASPECT_RATIO` in src/index.js if `imageAspectRatio` is present
      in global settings
      - added method for parsing and checking the value with returning the defaults, if
        something is not expected
    - added comment in `consts`, hinting that the value can differ
    - moved reading the value from `consts` into `imageHeight` to get the correct updated
      value when calling the method
    
    SVA-135

--- src/helpers/imageHelper.js
+++ src/helpers/imageHelper.js
@@ -1,13 +1,16 @@
 import _filter from 'lodash/filter';
+import _isArray from 'lodash/isArray';
+import _isString from 'lodash/isString';
+import _isInteger from 'lodash/isInteger';
 import { Dimensions } from 'react-native';
 
 import { consts } from '../config';
 
-const { IMAGE_ASPECT_RATIO } = consts;
-
 export const imageWidth = () => Dimensions.get('window').width;
 
 export const imageHeight = (width) => {
+  const { IMAGE_ASPECT_RATIO } = consts;
+
   // for accurate ratio in our view we need to calculate a factor with our current device
   // width for the image, to set a correct height
   const factor = width / IMAGE_ASPECT_RATIO.WIDTH;
@@ -31,4 +34,35 @@ export const mainImageOfMediaContents = (mediaContents) => {
   if (!images || !images.length) return null;
 
   return images[0].sourceUrl.url;
+};
+
+/**
+ * Parses a string with an image aspect ratio with 'width:height' format and returns
+ * and object with both values. If the value cannot be parsed, the default `IMAGE_ASPECT_RATIO`
+ * will be returned.
+ *
+ * @param {string} imageAspectRatio two values divided by a colon, something like '1:1'
+ *
+ * @return {object} both parsed values as an object, like { HEIGHT: 1, WIDTH: 1}
+ */
+export const parsedImageAspectRatio = (imageAspectRatio) => {
+  const { IMAGE_ASPECT_RATIO } = consts;
+
+  const splitImageAspectRatio =
+    imageAspectRatio && _isString(imageAspectRatio) && imageAspectRatio.split(':');
+
+  if (
+    splitImageAspectRatio &&
+    _isArray(splitImageAspectRatio) &&
+    splitImageAspectRatio.length === 2
+  ) {
+    const imageAspectRatioHeight = parseInt(splitImageAspectRatio[1], 10);
+    const imageAspectRatioWidth = parseInt(splitImageAspectRatio[0], 10);
+
+    if (_isInteger(imageAspectRatioHeight) && _isInteger(imageAspectRatioWidth)) {
+      return { HEIGHT: imageAspectRatioHeight, WIDTH: imageAspectRatioWidth };
+    }
+  }
+
+  return IMAGE_ASPECT_RATIO;
 };
