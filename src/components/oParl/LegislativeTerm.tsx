4748b8dcac57b02ca227dbdbc193a5693ac45d4d
Julian Kwast <jk@tpwd.de>
2021-01-26 17:47:51 +0100

    refactor(OParl): legislative term
    - legislative term now uses BodyPreview
    
    SVA-71, SVA-140

--- src/components/oParl/LegislativeTerm.tsx
+++ src/components/oParl/LegislativeTerm.tsx
@@ -1,10 +1,12 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { NavigationScreenProp } from 'react-navigation';
 
 import { texts } from '../../config';
 import { LegislativeTermData } from '../../types';
+import { BoldText } from '../Text';
 import { Wrapper } from '../Wrapper';
 import { LineEntry } from './LineEntry';
+import { BodyPreview } from './previews';
 import { DateSection, KeywordSection, WebRepresentation } from './sections';
 
 type Props = {
@@ -17,21 +19,16 @@ const { legislativeTerm } = texts.oparl;
 export const LegislativeTerm = ({ data, navigation }: Props) => {
   const { body, endDate, keyword, name, startDate, web } = data;
 
-  const onPressBody = useCallback(
-    () => body && navigation.push('OParlDetail', { id: body.id, title: body.name }),
-    [navigation, body]
-  );
-
   return (
     <Wrapper>
       <LineEntry left={legislativeTerm.name} right={name} />
       <DateSection endDate={endDate} startDate={startDate} />
-      <LineEntry
-        left={legislativeTerm.partOfBody}
-        lineThrough={body?.deleted}
-        onPress={onPressBody}
-        right={body?.name}
-      />
+      {!!body && (
+        <>
+          <BoldText>{legislativeTerm.partOfBody}</BoldText>
+          <BodyPreview data={body} navigation={navigation} />
+        </>
+      )}
       <KeywordSection keyword={keyword} />
       <WebRepresentation name={name ?? legislativeTerm.title} navigation={navigation} web={web} />
     </Wrapper>
