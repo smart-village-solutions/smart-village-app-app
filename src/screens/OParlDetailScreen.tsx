94b5b3334b1b8e84476d7490dff6ee43d8bbe90a
Julian Kwast <jk@tpwd.de>
2021-02-03 10:29:02 +0100

    refactor(OParl): use OParlComponent
    - removed switch statement for component selection in oparl screen
      - use OParlComponent instead
    
    SVA-71, SVA-140

--- src/screens/OParlDetailScreen.tsx
+++ src/screens/OParlDetailScreen.tsx
@@ -1,60 +1,13 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import { NavigationScreenProp } from 'react-navigation';
 
 import { HeaderLeft, RegularText, SafeAreaViewFlex, WrapperWithOrientation } from '../components';
-import {
-  AgendaItem,
-  Body,
-  Consultation,
-  File,
-  LegislativeTerm,
-  Location,
-  Meeting,
-  Membership,
-  Organization,
-  Paper,
-  Person,
-  System
-} from '../components/oParl';
+import { OParlComponent } from '../components/oParl';
 import { DummyData } from '../OParlDummyData';
-import { OParlObjectData, OParlObjectType } from '../types';
 
 type Props = {
   navigation: NavigationScreenProp<never>;
-};
-
-// eslint-disable-next-line complexity
-const getComponent = (data: OParlObjectData, navigation: NavigationScreenProp<never>) => {
-  switch (data.type) {
-    case OParlObjectType.AgendaItem:
-      return <AgendaItem data={data} navigation={navigation} />;
-    case OParlObjectType.Body:
-      return <Body data={data} navigation={navigation} />;
-    case OParlObjectType.Consultation:
-      return <Consultation data={data} navigation={navigation} />;
-    case OParlObjectType.File:
-      return <File data={data} navigation={navigation} />;
-    case OParlObjectType.LegislativeTerm:
-      return <LegislativeTerm data={data} navigation={navigation} />;
-    case OParlObjectType.Location:
-      return <Location data={data} navigation={navigation} />;
-    case OParlObjectType.Meeting:
-      return <Meeting data={data} navigation={navigation} />;
-    case OParlObjectType.Membership:
-      return <Membership data={data} navigation={navigation} />;
-    case OParlObjectType.Organization:
-      return <Organization data={data} navigation={navigation} />;
-    case OParlObjectType.Paper:
-      return <Paper data={data} navigation={navigation} />;
-    case OParlObjectType.Person:
-      return <Person data={data} navigation={navigation} />;
-    case OParlObjectType.System:
-      return <System data={data} navigation={navigation} />;
-    default:
-      // TODO: Add sensible fallback
-      return <View />;
-  }
 };
 
 export const OParlDetailScreen = ({ navigation }: Props) => {
@@ -71,7 +24,9 @@ export const OParlDetailScreen = ({ navigation }: Props) => {
   return (
     <SafeAreaViewFlex>
       <ScrollView>
-        <WrapperWithOrientation>{getComponent(data, navigation)}</WrapperWithOrientation>
+        <WrapperWithOrientation>
+          <OParlComponent data={data} navigation={navigation} />
+        </WrapperWithOrientation>
       </ScrollView>
     </SafeAreaViewFlex>
   );
