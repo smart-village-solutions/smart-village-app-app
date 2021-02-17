9250483df60d91c870301e67aa5c3937a2143f88
Julian Kwast <jk@tpwd.de>
2021-02-17 13:17:12 +0100

    fix(dropdown): position in lanscape mode
    - added orientation handling for dropdown
    
    SVA-180

--- src/components/DropdownSelect.js
+++ src/components/DropdownSelect.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { memo, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Dropdown from 'react-native-modal-dropdown';
 
@@ -9,69 +9,92 @@ import { Wrapper, WrapperRow, WrapperHorizontal } from './Wrapper';
 import { Icon } from './Icon';
 import { arrowDown, arrowUp } from '../icons';
 import { Label } from './Label';
+import { OrientationContext } from '../OrientationProvider';
 
-export const DropdownSelect = memo(
-  ({ data, setData, label, showSearch, searchInputStyle, renderSearch, searchPlaceholder }) => {
-    if (!data || !data.length) return null;
+export const DropdownSelect = ({
+  data,
+  setData,
+  label,
+  showSearch,
+  searchInputStyle,
+  renderSearch,
+  searchPlaceholder
+}) => {
+  const { orientation } = useContext(OrientationContext);
 
-    const [arrow, setArrow] = useState('down');
-    const selectedData = data.find((entry) => entry.selected);
-    const selectedIndex = data.findIndex((entry) => entry.selected);
-    const preselect = (index) => this[`dropdown${label}`].select(index);
+  const marginHorizontal =
+    orientation === 'portrait' ? normalize(14) : device.height * 0.15 + normalize(14);
 
-    useEffect(() => {
-      preselect(selectedIndex);
-    }, [selectedData]);
+  const adjustFrame = useCallback(
+    (styles) => ({
+      ...styles,
+      height: styles.height + normalize(36), // space for four entries
+      left: marginHorizontal,
+      marginTop: device.platform === 'android' ? -normalize(24) : 0
+    }),
+    [marginHorizontal]
+  );
 
-    return (
-      <View>
-        <WrapperHorizontal>
-          <Label>{label}</Label>
-        </WrapperHorizontal>
-        <Dropdown
-          ref={(ref) => (this[`dropdown${label}`] = ref)}
-          options={data.map((entry) => entry.value)}
-          dropdownStyle={styles.dropdownDropdown}
-          dropdownTextStyle={styles.dropdownDropdownText}
-          adjustFrame={(styles) => ({
-            ...styles,
-            height: styles.height + normalize(36), // space for four entries
-            left: normalize(14),
-            marginTop: device.platform === 'android' ? -normalize(24) : 0
-          })}
-          renderRow={(rowData, rowID, highlighted) => (
-            <Wrapper style={styles.dropdownRowWrapper}>
-              <RegularText primary={highlighted}>{rowData}</RegularText>
-            </Wrapper>
-          )}
-          renderSeparator={() => <View style={styles.dropdownSeparator} />}
-          onDropdownWillShow={() => setArrow('up')}
-          onDropdownWillHide={() => setArrow('down')}
-          onSelect={(index, value) => {
-            // only trigger onPress if a new selection is made
-            if (selectedData.value === value) return;
+  if (!data || !data.length) return null;
 
-            const updatedData = data.map((entry) => ({
-              ...entry,
-              selected: entry.value === value
-            }));
+  const [arrow, setArrow] = useState('down');
+  const selectedData = data.find((entry) => entry.selected);
+  const selectedIndex = data.findIndex((entry) => entry.selected);
+  const preselect = (index) => this[`dropdown${label}`].select(index);
 
-            setData(updatedData);
-          }}
-          showSearch={showSearch}
-          searchInputStyle={searchInputStyle}
-          renderSearch={renderSearch}
-          searchPlaceholder={searchPlaceholder}
-        >
-          <WrapperRow style={styles.dropdownTextWrapper}>
-            <RegularText>{selectedData.value}</RegularText>
-            <Icon xml={arrow == 'down' ? arrowDown(colors.primary) : arrowUp(colors.primary)} />
-          </WrapperRow>
-        </Dropdown>
-      </View>
-    );
-  }
-);
+  useEffect(() => {
+    preselect(selectedIndex);
+  }, [selectedData]);
+
+  return (
+    <View>
+      <WrapperHorizontal>
+        <Label>{label}</Label>
+      </WrapperHorizontal>
+      <Dropdown
+        ref={(ref) => (this[`dropdown${label}`] = ref)}
+        options={data.map((entry) => entry.value)}
+        dropdownStyle={[
+          styles.dropdownDropdown,
+          {
+            width:
+              (orientation === 'portrait' ? device.width : device.height) - 2 * marginHorizontal
+          }
+        ]}
+        dropdownTextStyle={styles.dropdownDropdownText}
+        adjustFrame={adjustFrame}
+        renderRow={(rowData, rowID, highlighted) => (
+          <Wrapper style={styles.dropdownRowWrapper}>
+            <RegularText primary={highlighted}>{rowData}</RegularText>
+          </Wrapper>
+        )}
+        renderSeparator={() => <View style={styles.dropdownSeparator} />}
+        onDropdownWillShow={() => setArrow('up')}
+        onDropdownWillHide={() => setArrow('down')}
+        onSelect={(index, value) => {
+          // only trigger onPress if a new selection is made
+          if (selectedData.value === value) return;
+
+          const updatedData = data.map((entry) => ({
+            ...entry,
+            selected: entry.value === value
+          }));
+
+          setData(updatedData);
+        }}
+        showSearch={showSearch}
+        searchInputStyle={searchInputStyle}
+        renderSearch={renderSearch}
+        searchPlaceholder={searchPlaceholder}
+      >
+        <WrapperRow style={styles.dropdownTextWrapper}>
+          <RegularText>{selectedData.value}</RegularText>
+          <Icon xml={arrow == 'down' ? arrowDown(colors.primary) : arrowUp(colors.primary)} />
+        </WrapperRow>
+      </Dropdown>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   dropdownTextWrapper: {
@@ -88,8 +111,7 @@ const styles = StyleSheet.create({
     shadowColor: colors.shadow,
     shadowOffset: { height: 5, width: 0 },
     shadowOpacity: 0.5,
-    shadowRadius: 3,
-    width: device.width - normalize(28) // substract two side padding
+    shadowRadius: 3
   },
   dropdownDropdownText: {
     color: colors.darkText,
