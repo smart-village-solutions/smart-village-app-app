2176d4698b732ce6b2f20f2dad8a866bab531f49
Julian Kwast <jk@tpwd.de>
2021-02-23 12:25:57 +0100

    feat(image): add refresh interval
    - added option to set a refresh interval for the image component
    - added parsing of refresh interval for ImagesCarousel
    
    SVA-182

--- src/hooks/TimeHooks.ts
+++ src/hooks/TimeHooks.ts
@@ -21,3 +21,20 @@ export const useRefreshTime = (
 
   return refreshTime;
 };
+
+// counts up a number on a set interval
+export const useInterval = (ms?: number) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!ms) return;
+
+    const id = setInterval(() => {
+      setCount((value) => value + 1);
+    }, ms);
+
+    return () => clearInterval(id);
+  }, [ms, setCount]);
+
+  return count;
+};
