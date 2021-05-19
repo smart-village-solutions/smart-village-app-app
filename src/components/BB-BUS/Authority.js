b3c606488e9fa2dc0ee14264d3495bd44cc0e0d5
Julian Kwast <jk@tpwd.de>
2021-05-19 17:10:03 +0200

    refactor(bb bus): detail screen
    - useQuery instead of query component
    - simplified collapsible logic
      - added new `Block` component that handles collapsing
      - reused this `Block` component in `Authority.js` and `Persons.js`
    - extracted smaller functions and components out of the detail screen

--- src/components/BB-BUS/Authority.js
+++ src/components/BB-BUS/Authority.js
@@ -1,101 +1,68 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { ListItem } from 'react-native-elements';
 
-import { colors, normalize, texts } from '../../config';
+import { texts } from '../../config';
 import { trimNewLines } from '../../helpers';
 import { HtmlView } from '../HtmlView';
 import { InfoCard } from '../infoCard';
 import { BoldText, RegularText } from '../Text';
-import { Title } from '../Title';
-import { Touchable } from '../Touchable';
 import { Wrapper, WrapperWithOrientation } from '../Wrapper';
+import { Block } from './Block';
 import { getAddress, getContact } from './helpers';
 
-/* eslint-disable complexity */
-/* NOTE: we need to check a lot for presence, so this is that complex */
-export const Authority = ({ data, accordion, onPress, bottomDivider, openWebScreen }) => {
-  const {
-    id,
-    name,
-    addresses,
-    communications,
-    openingHours,
-    elevator,
-    wheelchairAccessible
-  } = data;
+export const Authority = ({ data, bottomDivider, openWebScreen }) => {
+  const { name, addresses, communications, openingHours, elevator, wheelchairAccessible } = data;
 
   const address = getAddress(addresses);
   const contact = getContact(communications);
 
   return (
-    <View key={id}>
-      <ListItem
-        title={<Title small>{name}</Title>}
-        bottomDivider={bottomDivider}
-        topDivider
-        containerStyle={styles.sectionTitle}
-        rightIcon={<Title>{accordion[id] ? '－' : '＋'}</Title>}
-        onPress={onPress}
-        delayPressIn={0}
-        Component={Touchable}
-      />
-      {!!accordion[id] && (
+    <Block title={name} bottomDivider={bottomDivider}>
+      <WrapperWithOrientation>
+        <InfoCard address={address} contact={contact} openWebScreen={openWebScreen} />
+      </WrapperWithOrientation>
+      {(!!openingHours || elevator !== undefined || wheelchairAccessible !== undefined) && (
         <WrapperWithOrientation>
-          <InfoCard address={address} contact={contact} openWebScreen={openWebScreen} />
+          <Wrapper style={styles.wrapperWithoutTopPadding}>
+            {!!openingHours && (
+              <View>
+                <BoldText>{texts.bbBus.authority.openingTime}:</BoldText>
+                <HtmlView html={trimNewLines(openingHours)} openWebScreen={openWebScreen} />
+              </View>
+            )}
+            {elevator !== undefined && (
+              <View>
+                <BoldText>{texts.bbBus.authority.elevator}:</BoldText>
+                <RegularText>{elevator ? 'ja' : 'nein'}</RegularText>
+              </View>
+            )}
+            {elevator !== undefined && wheelchairAccessible !== undefined && (
+              <View>
+                <RegularText />
+              </View>
+            )}
+            {wheelchairAccessible !== undefined && (
+              <View>
+                <BoldText>{texts.bbBus.authority.wheelchairAccessible}:</BoldText>
+                <RegularText>{wheelchairAccessible ? 'ja' : 'nein'}</RegularText>
+              </View>
+            )}
+          </Wrapper>
         </WrapperWithOrientation>
       )}
-      {!!accordion[id] &&
-        (!!openingHours || elevator !== undefined || wheelchairAccessible !== undefined) && (
-          <WrapperWithOrientation>
-            <Wrapper style={styles.wrapperWithoutTopPadding}>
-              {!!openingHours && (
-                <View>
-                  <BoldText>{texts.bbBus.authority.openingTime}:</BoldText>
-                  <HtmlView html={trimNewLines(openingHours)} openWebScreen={openWebScreen} />
-                </View>
-              )}
-              {elevator !== undefined && (
-                <View>
-                  <BoldText>{texts.bbBus.authority.elevator}:</BoldText>
-                  <RegularText>{elevator ? 'ja' : 'nein'}</RegularText>
-                </View>
-              )}
-              {elevator !== undefined && wheelchairAccessible !== undefined && (
-                <View>
-                  <RegularText />
-                </View>
-              )}
-              {wheelchairAccessible !== undefined && (
-                <View>
-                  <BoldText>{texts.bbBus.authority.wheelchairAccessible}:</BoldText>
-                  <RegularText>{wheelchairAccessible ? 'ja' : 'nein'}</RegularText>
-                </View>
-              )}
-            </Wrapper>
-          </WrapperWithOrientation>
-        )}
-    </View>
+    </Block>
   );
 };
-/* eslint-enable complexity */
 
 const styles = StyleSheet.create({
   wrapperWithoutTopPadding: {
     paddingTop: 0
-  },
-  sectionTitle: {
-    backgroundColor: colors.transparent,
-    paddingHorizontal: normalize(14),
-    paddingVertical: normalize(12)
   }
 });
 
 Authority.propTypes = {
   data: PropTypes.object.isRequired,
-  accordion: PropTypes.object.isRequired,
-  onPress: PropTypes.func.isRequired,
   bottomDivider: PropTypes.bool.isRequired,
   openWebScreen: PropTypes.func.isRequired
 };
--- src/components/BB-BUS/Persons.js
+++ src/components/BB-BUS/Persons.js
@@ -1,108 +1,86 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { StyleSheet } from 'react-native';
 import { pull } from 'lodash';
 
-import { colors, normalize, texts } from '../../config';
-import { InfoBox, RegularText, Title, Touchable, Wrapper } from '..';
+import { InfoBox, RegularText, Wrapper } from '..';
 import { WrapperWithOrientation } from '../Wrapper';
 import { getAddress, getContact } from './helpers';
 import { InfoCard } from '../infoCard';
+import { Block } from './Block';
 
-/* eslint-disable complexity */
-/* NOTE: we need to check a lot for presence, so this is that complex */
-export const Persons = ({ data, accordion, onPress, openWebScreen }) => {
-  const { id, persons } = data;
+export const Persons = ({ data, openWebScreen }) => {
+  const { persons } = data;
 
   return (
-    <View>
-      <ListItem
-        title={<Title small>{texts.bbBus.employees}</Title>}
-        bottomDivider
-        topDivider
-        containerStyle={styles.sectionTitle}
-        rightIcon={<Title>{accordion[id] ? '－' : '＋'}</Title>}
-        onPress={onPress}
-        delayPressIn={0}
-        Component={Touchable}
-      />
-      {!!accordion[id] &&
-        persons.map((person) => {
-          if (person.object) person.person = person.object;
+    <Block title={name}>
+      {persons.map((person) => {
+        if (person.object) person.person = person.object;
 
-          const {
-            id,
-            title,
-            firstName,
-            lastName,
-            position,
-            department,
-            room,
-            addresses,
-            communication // NOTE: the main server returns singular here instead of plural
-          } = person.person;
+        const {
+          id,
+          title,
+          firstName,
+          lastName,
+          position,
+          department,
+          room,
+          addresses,
+          communication // NOTE: the main server returns singular here instead of plural
+        } = person.person;
 
-          // set plural from singular
-          let communications = communication;
+        // set plural from singular
+        let communications = communication;
 
-          // if the result came from Directus, there is no singular and we need the plural directly
-          if (!communications) communications = person.person.communications;
+        // if the result came from Directus, there is no singular and we need the plural directly
+        if (!communications) communications = person.person.communications;
 
-          const address = getAddress(addresses);
-          const contact = getContact(communications);
+        const address = getAddress(addresses);
+        const contact = getContact(communications);
 
-          const name = pull([title, firstName, lastName], undefined).join(' ');
+        const name = pull([title, firstName, lastName], undefined).join(' ');
 
-          return (
-            <WrapperWithOrientation key={id}>
-              {(!!name || !!position || !!department || !!room) && (
-                <Wrapper style={styles.wrapperWithoutBottomPadding}>
-                  {!!name && (
-                    <InfoBox>
-                      <RegularText>{name}</RegularText>
-                    </InfoBox>
-                  )}
-                  {!!position && (
-                    <InfoBox>
-                      <RegularText>Position: {position}</RegularText>
-                    </InfoBox>
-                  )}
-                  {!!department && (
-                    <InfoBox>
-                      <RegularText>Abteilung: {department}</RegularText>
-                    </InfoBox>
-                  )}
-                  {!!room && (
-                    <InfoBox>
-                      <RegularText>Raum: {room}</RegularText>
-                    </InfoBox>
-                  )}
-                </Wrapper>
-              )}
-              <InfoCard address={address} contact={contact} openWebScreen={openWebScreen} />
-            </WrapperWithOrientation>
-          );
-        })}
-    </View>
+        return (
+          <WrapperWithOrientation key={id}>
+            {(!!name || !!position || !!department || !!room) && (
+              <Wrapper style={styles.wrapperWithoutBottomPadding}>
+                {!!name && (
+                  <InfoBox>
+                    <RegularText>{name}</RegularText>
+                  </InfoBox>
+                )}
+                {!!position && (
+                  <InfoBox>
+                    <RegularText>Position: {position}</RegularText>
+                  </InfoBox>
+                )}
+                {!!department && (
+                  <InfoBox>
+                    <RegularText>Abteilung: {department}</RegularText>
+                  </InfoBox>
+                )}
+                {!!room && (
+                  <InfoBox>
+                    <RegularText>Raum: {room}</RegularText>
+                  </InfoBox>
+                )}
+              </Wrapper>
+            )}
+            <InfoCard address={address} contact={contact} openWebScreen={openWebScreen} />
+          </WrapperWithOrientation>
+        );
+      })}
+    </Block>
   );
 };
-/* eslint-enable complexity */
 
 const styles = StyleSheet.create({
   wrapperWithoutBottomPadding: {
     paddingBottom: 0
-  },
-  sectionTitle: {
-    backgroundColor: colors.transparent,
-    paddingHorizontal: normalize(14),
-    paddingVertical: normalize(12)
   }
 });
 
 Persons.propTypes = {
   data: PropTypes.object.isRequired,
-  accordion: PropTypes.object.isRequired,
-  onPress: PropTypes.func.isRequired,
   openWebScreen: PropTypes.func.isRequired
 };
