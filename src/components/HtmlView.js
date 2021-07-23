8f166d6532b60431fcc8d0839f8c2ec43335e6fc
Julian Kwast <jk@tpwd.de>
2021-07-23 15:59:14 +0200

    chore(html table): exchange dependency
    - the react-native-render-html-table-bridge is outdated and deprecated
      - instead added a version of @native-html/table-plugin that is compatible with our version of react-native-render-html
    - adjusted some stylings to make it close to how it was before

--- src/components/HtmlView.js
+++ src/components/HtmlView.js
@@ -2,49 +2,41 @@ import PropTypes from 'prop-types';
 import React, { memo } from 'react';
 import HTML from 'react-native-render-html';
 import {
-  IGNORED_TAGS,
   alterNode,
-  makeTableRenderer,
+  cssRulesFromSpecs,
   defaultTableStylesSpecs,
-  cssRulesFromSpecs
-} from 'react-native-render-html-table-bridge';
+  IGNORED_TAGS,
+  makeTableRenderer
+} from '@native-html/table-plugin';
 import { WebView } from 'react-native-webview';
 
 import { colors, consts, normalize, styles } from '../config';
 import { imageWidth, openLink } from '../helpers';
 
-const tableCssRules =
+const cssRules =
   cssRulesFromSpecs({
     ...defaultTableStylesSpecs,
     linkColor: colors.primary,
-    // TODO: font family was not working at the moment with following implementation
-    //       https://github.com/jsamr/react-native-render-html-table-bridge/blob/master/readme.md#how-to-load-custom-fonts
+    // TODO: font family documentation
+    //      https://github.com/native-html/plugins/tree/rnrh/4.x#how-to-load-custom-fonts
     // fontFamily: 'regular',
     thColor: colors.lightestText,
     trOddBackground: colors.lightestText,
     trOddColor: colors.darkText,
     trEvenBackground: colors.lightestText,
-    trEvenColor: colors.darkText
+    trEvenColor: colors.darkText,
+    outerBorderWidthPx: 1,
+    outerBorderColor: '#b5b5b5',
+    columnsBorderWidthPx: 1
   }) +
   `
 :root {
   font-size: ${normalize(13)}px;
 }
-th {
-  border-left: 0.25px solid ${'#3f5c7a'};
-  border-top: 0.25px solid ${'#3f5c7a'};
-}
-td {
-  border-left: 0.25px solid ${'#b5b5b5'};
-  border-top: 0.25px solid ${'#b5b5b5'};
-}
 `;
 
 const renderers = {
-  table: makeTableRenderer({
-    WebViewComponent: WebView,
-    cssRules: tableCssRules
-  })
+  table: makeTableRenderer({ WebView, cssRules })
 };
 
 const htmlConfig = {
