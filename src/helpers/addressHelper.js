d0da873906c149b75cbe1f01588b834c85ff3f93
Julian Kwast <jk@tpwd.de>
2021-10-20 15:22:19 +0200

    chore(address helper): rework multiline formatting
    - fixed issues with single line format along the way
    
    SVA-323

--- src/helpers/addressHelper.js
+++ src/helpers/addressHelper.js
@@ -2,25 +2,16 @@ export const formatAddress = (address) => {
   if (!address) return;
 
   const { city, street, zip, addition } = address;
-  let readableAddress = '';
 
   if (!city && !street && !zip && !addition) return;
 
-  // build the address in multiple steps to check every data before rendering
-  if (addition?.length) {
-    readableAddress += `${addition}${'\n'}`;
-  }
-  if (street?.length) {
-    readableAddress += `${street},${'\n'}`;
-  }
-  if (zip?.length) {
-    readableAddress += `${zip} `;
-  }
-  if (city?.length) {
-    readableAddress += city;
-  }
+  // the city and zip are supposed to be in one line together
+  const cityPart = [zip, city].filter((s) => s?.length).join(' ');
 
-  return readableAddress;
+  // add an extra ',' to the street, if it is defined
+  return [addition, street?.length ? `${street},` : undefined, cityPart]
+    .filter((s) => s?.length)
+    .join('\n');
 };
 
 export const formatAddressSingleLine = (address) => {
@@ -30,8 +21,8 @@ export const formatAddressSingleLine = (address) => {
 
   if (!city && !street && !zip && !addition) return;
 
-  const streetPart = [addition, street].join(' ');
-  const cityPart = [zip, city].join(' ');
+  const streetPart = [addition, street].filter((s) => s?.length).join(' ');
+  const cityPart = [zip, city].filter((s) => s?.length).join(' ');
 
-  return [streetPart, cityPart].join(', ');
+  return [streetPart, cityPart].filter((s) => s?.length).join(', ');
 };
