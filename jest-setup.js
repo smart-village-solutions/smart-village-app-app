fcfa24c950ae1e42fa10e674fb2279c896fa7c93
Julian Kwast <jk@tpwd.de>
2021-11-15 16:36:31 +0100

    chore(expo 43): update jest setup
    SVA-351

--- jest-setup.js
+++ jest-setup.js
@@ -1,5 +1,8 @@
 import 'whatwg-fetch';
 import 'react-native-gesture-handler/jestSetup';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
