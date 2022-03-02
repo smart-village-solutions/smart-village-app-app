14b84251fbe185e129243897faf96ddd65705879
Daniel Molnar <dm@tpwd.de>
2022-03-02 21:28:01 +0100

    chore(volunteer): comment out dummy entries on profile index screen
    HDVT-39

--- src/helpers/parser/volunteer/profileData.js
+++ src/helpers/parser/volunteer/profileData.js
@@ -5,13 +5,13 @@ export const myProfile = () => [
     id: 1,
     title: 'Persönliche Daten',
     onPress: (navigation) => navigation?.navigate(ScreenName.VolunteerMe)
-  },
-  {
-    id: 2,
-    title: 'Einstellungen'
-  },
-  {
-    id: 3,
-    title: 'Gespeichert'
   }
+  // {
+  //   id: 2,
+  //   title: 'Einstellungen'
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Gespeichert'
+  // }
 ];
