0499af18c94e1eaa3637d20d95fd5ed900262741
Daniel Molnar <dm@tpwd.de>
2022-06-02 21:12:46 +0200

    setup: development build with `expo-dev-client`
    - installed `expo-dev-client` as documented here:
      https://docs.expo.dev/development/getting-started/#installing--expo-dev-client--in-your-project
    - adjusted eas.json as there was an error saying:
      `Too long chain of profile extensions, make sure "extends" keys do not make a cycle`
    - added `ios.bundleIdentifier` and `android.package` as local run scripts expected them
    - automatically changed things after running `yarn ios` like adjustment of package.json:
      `Removed "main": "node_modules/expo/AppEntry.js" from package.json because we recommend using index.js as main instead`
      or adding `react-dom` and `react-native-web` to the dependencies
    - prepared custom expo client with `yarn android` locally -> worked
      - was running automatically on an android emulator
    - prepared custom expo client with `yarn ios` locally -> failed
      - this needs a proper setup Xcode, which seems to fail for me as my is outdated because of an old macOS
      - in addition i needed to remove `"EXCLUDED_ARCHS[sdk=iphonesimulator*]" = "arm64 ";` in `project.pbxproj` files,
        to make everything compile sucessfully except for @react-native-community/datetimepicker
        - this package seems to failed because of outdated macOS & Xcode
    - building with `yarn build:ios-simulator` was successful on expo servers but therefore
      it was needed to temporarily comment out the gitignorance of `/src/config/secrets.js`
      as it would be missing on the server processes otherwise
      - you can find the different development builds in our expo account on expo.dev like previous `expo build`s
    - after downloading the custom client follow these instructions:
      https://docs.expo.dev/build-reference/simulators/#installing-your-build-on-the-simulator
      - when running on the simulator everything should work 99% as known from the Expo Go app
    - to start developing run `yarn dev` which will create a local server for the custom client
    
    SVA-444

---
+++ android/app/src/main/java/de/dev/smartvillage/app/MainActivity.java
@@ -0,0 +1,54 @@
+package de.dev.smartvillage.app;
+import expo.modules.devmenu.react.DevMenuAwareReactActivity;
+import android.content.Intent;
+import expo.modules.devlauncher.DevLauncherController;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+import expo.modules.ReactActivityDelegateWrapper;
+
+public class MainActivity extends DevMenuAwareReactActivity {
+
+  @Override
+  public void onNewIntent(Intent intent) {
+      if (DevLauncherController.tryToHandleIntent(this, intent)) {
+         return;
+      }
+      super.onNewIntent(intent);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // Set the theme to AppTheme BEFORE onCreate to support 
+    // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
+    setTheme(R.style.AppTheme);
+    super.onCreate(null);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "main";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return DevLauncherController.wrapReactActivityDelegate(this, () -> new ReactActivityDelegateWrapper(
+      this,
+      new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    }));
+  }
+}
