9b307399834b7b637c51ff084926b6431c6ba37c
Arda Senturk <ase@tpwd.de>
2022-07-08 15:25:31 +0200

    fix(storage name): change directory
    - changed the name of the storage to the
      name of the file to resolve the error of
      objects not displaying properly
      -  a note has been added to the file so
          that it can work at the desired performance.
          will be updated later
    
    SVA-633

--- src/helpers/augmentedReality/storageNameCreator.js
+++ src/helpers/augmentedReality/storageNameCreator.js
@@ -1,6 +1,25 @@
 export const storageNameCreator = ({ dataItem, objectItem }) => {
-  const { id: objectId } = dataItem;
-  const { title, type, id } = objectItem;
+  const { id: objectId, title: objectTitle } = dataItem;
+  const { title, type } = objectItem;
 
-  return `${objectId}-${title}${id}.${type}`;
+  /*
+	each texture file has a special name. viro uses these special names. 
+	we need to save them in the file directory with these names. the problem 
+	here is that the texture names of different objects may be the same. if 
+	this happens, when downloading the file, it saves it in place of the 
+	previous downloaded one and the object cannot be loaded properly. 
+	we need a different directory for each object. the file system does not 
+	allow us to create new directories. we have to find a different way!
+	*/
+
+  /*
+	  TODO: error when creating a directory
+         the file directory to be created is specified in the comment line
+   	Expo-FileSystem Document:
+    	https://docs.expo.dev/versions/latest/sdk/filesystem/#filesystemdocumentdirectory
+
+		return `${objectTitle.replace(/\s+/g, '')}_${objectId}/${title}.${type}`;
+	 */
+
+  return `${title}.${type}`;
 };
