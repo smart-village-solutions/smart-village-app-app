eb1b2d490fa5e88b4a8cb6d3a7658b59c9b877b0
Arda Senturk <ase@tpwd.de>
2022-07-08 15:38:54 +0200

    feat(ar object): parse ar object
    - added parser to place all augmented
      reality objects where `viro` wants them
    
    SVA-633

--- src/screens/augmentedReality/ARShowScreen.js
+++ src/screens/augmentedReality/ARShowScreen.js
@@ -1,7 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { RegularText, SafeAreaViewFlex } from '../../components';
+import { LoadingSpinner, RegularText } from '../../components';
+
+export const ARShowScreen = ({ navigation, route }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const data = route?.params?.data ?? [];
+  const [object, setObject] = useState();
+  const index = route?.params?.index;
+
+  useEffect(() => {
+    parser();
+  }, []);
+
+  const parser = async () => {
+    await objectParser({ item: data[index], setObject });
+    setIsLoading(false);
+  };
+
+  if (isLoading || !object) return <LoadingSpinner loading />;
 
 export const ARShowScreen = ({ navigation }) => {
   return (
@@ -12,6 +32,25 @@ export const ARShowScreen = ({ navigation }) => {
   );
 };
 
+const objectParser = async ({ item, setObject }) => {
+  let parsedObject = {};
+
+  item.localUris?.find((item) => {
+    if (item.type === 'vrx') {
+      parsedObject.vrx = item.uri;
+    }
+    if (item.type === 'png') {
+      parsedObject.png = item.uri;
+    }
+    if (item.type === 'mp3') {
+      parsedObject.mp3 = item.uri;
+    }
+  });
+
+  setObject(parsedObject);
+};
 ARShowScreen.propTypes = {
-  navigation: PropTypes.object.isRequired
+  navigation: PropTypes.object.isRequired,
+  route: PropTypes.object.isRequired
+};
 };
