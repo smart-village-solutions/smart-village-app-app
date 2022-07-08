e971ad75f40c43dad9e42e5c6e616dd58ee0d678
Arda Senturk <ase@tpwd.de>
2022-07-08 15:52:49 +0200

    feat(ar): show ar modal
    - added `viro` components required to display
      the test object we have
    - added `ViroAmbientLight` component to
      show 3d objects
    - added `diffuseTexture` object in
      `ViroMaterials.createMaterials` to add texture
      to object
    - added callback functions in `AugmentedRealityView`
      to indicate whether the object has started loading,
       is loading or has encountered an error
    - added `RegularText` for test purposes to go to the
      previous page from the ar show screen
    
    SVA-633

--- src/screens/augmentedReality/ARShowScreen.js
+++ src/screens/augmentedReality/ARShowScreen.js
@@ -1,7 +1,15 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import {
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroMaterials
+} from '@viro-community/react-viro';
 
-import { LoadingSpinner, RegularText, SafeAreaViewFlex } from '../../components';
+import { LoadingSpinner, RegularText } from '../../components';
 
 export const ARShowScreen = ({ navigation, route }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,10 +29,60 @@ export const ARShowScreen = ({ navigation, route }) => {
   if (isLoading || !object) return <LoadingSpinner loading />;
 
   return (
-    <SafeAreaViewFlex>
-      <RegularText>AR Show Screen</RegularText>
-      <RegularText onPress={() => navigation.goBack()}>Zurück</RegularText>
-    </SafeAreaViewFlex>
+    <View style={styles.f1}>
+      <ViroARSceneNavigator
+        initialScene={{
+          scene: AugmentedRealityView
+        }}
+        viroAppProps={{ object }}
+        style={styles.f1}
+      />
+      <RegularText style={styles.backButton} onPress={() => navigation.goBack()}>
+        Zurück
+      </RegularText>
+    </View>
+  );
+};
+
+const AugmentedRealityView = ({ sceneNavigator }) => {
+  const { object } = sceneNavigator.viroAppProps;
+
+  ViroMaterials.createMaterials({
+    SVA: {
+      diffuseTexture: {
+        uri: object.png
+      }
+    }
+  });
+
+  const handleLoadStart = () => {
+    console.warn('OBJ loading has started');
+  };
+  const handleLoadEnd = () => {
+    console.warn('OBJ loading has finished');
+  };
+  const handleError = (event) => {
+    console.warn('OBJ loading failed with error: ' + event.nativeEvent.error);
+  };
+
+  return (
+    <ViroARScene dragType="FixedToWorld">
+      <ViroAmbientLight color={'#fff'} />
+
+      <Viro3DObject
+        source={{
+          uri: object.vrx
+        }}
+        resources={['SVA']}
+        type="VRX"
+        position={[0, -1, -5]}
+        scale={[0.02, 0.02, 0.02]}
+        rotation={[0, 0, 0]}
+        onLoadStart={handleLoadStart}
+        onLoadEnd={handleLoadEnd}
+        onError={(event) => handleError(event)}
+      />
+    </ViroARScene>
   );
 };
 
@@ -46,7 +104,23 @@ const objectParser = async ({ item, setObject }) => {
   setObject(parsedObject);
 };
 
+var styles = StyleSheet.create({
+  backButton: {
+    position: 'absolute',
+    right: 10,
+    top: 100,
+    zIndex: 1
+  },
+  f1: {
+    flex: 1
+  }
+});
+
 ARShowScreen.propTypes = {
   navigation: PropTypes.object.isRequired,
   route: PropTypes.object.isRequired
+};
+
+AugmentedRealityView.propTypes = {
+  sceneNavigator: PropTypes.object
 };
