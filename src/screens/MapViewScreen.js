bee61071b79b5f78ebf452d4eee97424d012b722
Arda Senturk <ase@tpwd.de>
2022-07-25 14:37:01 +0200

    feat(ar): show art information below the map
    - added artwork information window shown below
      the map when clicking on the marker on the full
      page map
    - used `navigationToArtworksDetailScreen` helper
      to navigate to `artworkDetailScreen` from the
      opened detail window
    
    SVA-634

--- src/screens/MapViewScreen.js
+++ src/screens/MapViewScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
 import { Map, RegularText, Wrapper, WrapperRow, WrapperVertical } from '../components';
 import { colors, Icon, normalize } from '../config';
+import { navigationToArtworksDetailScreen } from '../helpers';
 
 export const MapViewScreen = ({ navigation, route }) => {
   const {
@@ -14,20 +15,65 @@ export const MapViewScreen = ({ navigation, route }) => {
     showsUserLocation
   } = route?.params;
 
+  /* the improvement in the next comment line has been added for augmented reality feature. */
+  const { data, refetch } = route?.params?.augmentedRealityData ?? [];
+
+  const [modelId, setModelId] = useState();
+  const [modelData, setModelData] = useState();
+
+  useEffect(() => {
+    navigationToArtworksDetailScreen({ data, isShow: true, modelId, setModelData });
+  }, [modelId]);
+  /* end of augmented reality feature */
 
   return (
+    <>
       <Map
         {...{
           isMaximizeButtonVisible,
           locations,
           mapStyle: styles.mapStyle,
+          onMarkerPress: isAugmentedReality ? setModelId : onMarkerPress,
           showsUserLocation
         }}
       />
+      {isAugmentedReality && modelData && (
+        <Wrapper>
+          <WrapperRow spaceBetween>
+            <View style={styles.augmentedRealityInfoContainer}>
+              <RegularText big>{modelData.title}</RegularText>
+              <RegularText small>{modelData.locationInfo}</RegularText>
+              <WrapperVertical>
+                <RegularText placeholder numberOfLines={2}>
+                  {modelData.description}
+                </RegularText>
+              </WrapperVertical>
+            </View>
+
+            <TouchableOpacity
+              onPress={() =>
+                navigationToArtworksDetailScreen({
+                  data,
+                  isNavigation: true,
+                  modelId,
+                  navigation,
+                  refetch
+                })
+              }
+            >
+              <Icon.ArrowRight size={normalize(30)} color={colors.primary} />
+            </TouchableOpacity>
+          </WrapperRow>
+        </Wrapper>
+      )}
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  augmentedRealityInfoContainer: {
+    width: '90%'
+  },
   mapStyle: {
     width: '100%',
     height: '100%'
