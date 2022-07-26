1a98fc7abe7cd59a26caff106156a32bf72ee49e
Daniel Molnar <dm@tpwd.de>
2022-07-26 13:47:04 +0200

    wip: update dropdown list when switching screens
    - 1. problem: no dropdown shown when navigating from "Veranstaltungen" to "Nachrichten"
    - 2. problem: dropdown values do not change when navigating from "Nachrichten" to "Meldungen" or other way around
    - 3. problem: filter on "Veranstaltungen" is not shown until data loaded when navigating from "Nachrichten" to "Veranstaltungen"
    
    HDVT-73

--- src/components/DropdownHeader.js
+++ src/components/DropdownHeader.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
-import React, { useEffect, useRef, useState } from 'react';
 import _uniqBy from 'lodash/uniqBy';
+import PropTypes from 'prop-types';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { texts } from '../config';
-import { QUERY_TYPES } from '../queries';
 import { usePermanentFilter } from '../hooks';
+import { QUERY_TYPES } from '../queries';
 
 import { DropdownSelect } from './DropdownSelect';
 import { Wrapper } from './Wrapper';
@@ -17,36 +17,36 @@ export const DropdownHeader = ({ query, queryVariables, data, updateListData }) 
 
   const { state: excludedDataProviders } = usePermanentFilter();
 
-  const dropdownInitialData = (query) => {
+  const dropdownInitialData = useCallback(() => {
     switch (query) {
       case QUERY_TYPES.EVENT_RECORDS:
-        return (
-          data?.categories?.length &&
-          _uniqBy(data.categories, 'name')
-            .filter((category) => !!category.upcomingEventRecordsCount)
-            .map((category, index) => ({
-              index: index + 1,
-              id: category.id,
-              value: category.name,
-              selected: category.id === queryVariables.categoryId
-            }))
-        );
+        return data?.categories?.length
+          ? _uniqBy(data.categories, 'name')
+              .filter((category) => !!category.upcomingEventRecordsCount)
+              .map((category, index) => ({
+                index: index + 1,
+                id: category.id,
+                value: category.name,
+                selected: category.id === queryVariables.categoryId
+              }))
+          : [];
       case QUERY_TYPES.NEWS_ITEMS: {
         const filteredDataProviders = data?.dataProviders?.filter(
           (dataProvider) => !excludedDataProviders.includes(dataProvider.id)
         );
 
-        return (
-          filteredDataProviders?.length &&
-          _uniqBy(filteredDataProviders, 'name').map((dataProvider, index) => ({
-            index: index + 1,
-            value: dataProvider.name,
-            selected: dataProvider.name === queryVariables.dataProvider
-          }))
-        );
+        return filteredDataProviders?.length
+          ? _uniqBy(filteredDataProviders, 'name').map((dataProvider, index) => ({
+              index: index + 1,
+              value: dataProvider.name,
+              selected: dataProvider.name === queryVariables.dataProvider
+            }))
+          : [];
       }
+      default:
+        return [];
     }
-  };
+  }, [query, queryVariables, data, updateListData, excludedDataProviders]);
 
   // check if there is something set in the certain `queryVariables`
   // if not, - Alle - will be selected in the `dropdownData`
@@ -64,14 +64,17 @@ export const DropdownHeader = ({ query, queryVariables, data, updateListData }) 
       index: 0,
       value: '- Alle -',
       selected: selectedInitial
-    },
-    ...(dropdownInitialData(query) || [])
+    }
   ]);
-
-  const selectedDropdownData = dropdownData.find((entry) => entry.selected);
 
   // https://medium.com/swlh/prevent-useeffects-callback-firing-during-initial-render-the-armchair-critic-f71bc0e03536
   const initialRender = useRef(true);
+
+  const selectedDropdownData = dropdownData?.find((entry) => entry.selected);
+
+  useEffect(() => {
+    setDropdownData((oldDropdownData) => [...oldDropdownData, ...dropdownInitialData()]);
+  }, [query]);
 
   // influence list data when changing selected dropdown value
   // call update of the list with the selected data provider
@@ -83,7 +86,7 @@ export const DropdownHeader = ({ query, queryVariables, data, updateListData }) 
       // inside of updateListData
       updateListData(!!selectedDropdownData.index && selectedDropdownData[selectedKey]);
     }
-  }, [selectedDropdownData.value]);
+  }, [selectedDropdownData?.value]);
 
   // do not show a filter, if there is just one entry (Alle + Item = 2)
   if (dropdownData.length <= 2) return null;
