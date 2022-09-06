72f0bfdfcd3449dce3164356bdac5a04f58b2826
Arda Senturk <ase@tpwd.de>
2022-09-06 16:18:26 +0200

    feat(volunteer): add different types to the message array
    - updated types in `messageArray`
      object for different media types
    - added `userInfo` function for the
      information needed to create a user
      avatar
    - added `mediaParser` function to
      separate different media types and
      create an array for each type
    - added `textParser` function to separate
      the text message in case there is a
      text message with medias
    - split according to `](file-guid:` to
      find the dataType in the `mediaParser`
      function
      - because all the data comes in the
         content string and we need to organise it
      - split all data into different types using
         different regexes
      - within the `urisWithDataType` array,
         all media are merged by creating a
         downloadable link
      - `mediaParser` function returned an
         object sorted by different file types
    
    HDVT-71

--- src/components/volunteer/VolunteerMessage.tsx
+++ src/components/volunteer/VolunteerMessage.tsx
@@ -3,14 +3,20 @@ import React, { useEffect, useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 
 import { Chat } from '../Chat';
-import { volunteerProfileImage, volunteerUserData } from '../../helpers';
-import { conversationNewEntry, conversationRecipients } from '../../queries/volunteer';
+import { consts } from '../../config';
+import {
+  conversationNewEntry,
+  conversationRecipients,
+  conversationUpload
+} from '../../queries/volunteer';
 import { VolunteerConversation } from '../../types';
+import { volunteerMessageMedia, volunteerProfileImage, volunteerUserData } from '../../helpers';
+
+const { GUID_REGEX, IMAGE_TYPE_REGEX, PDF_TYPE_REGEX, VIDEO_TYPE_REGEX } = consts;
 
 export const VolunteerMessage = ({
   data,
-  conversationId,
-  refetch
+  conversationId
 }: {
   data: {
     results: [
@@ -26,7 +32,6 @@ export const VolunteerMessage = ({
     ];
   };
   conversationId: number;
-  refetch: () => void;
 }) => {
   const [messageData, setMessageData] = useState<any>([]);
   const [currentUserId, setCurrentUserId] = useState<string | null>(null);
@@ -44,40 +49,39 @@ export const VolunteerMessage = ({
 
   useEffect(() => {
     const messageArray: {
-      text: string;
+      text?: string | undefined;
       createdAt: string;
       updatedAt: string;
       _id: number;
-      user: { _id: number; name: any; avatar: string };
+      image: { type: string | undefined; uri: string | undefined }[] | undefined;
+      pdf: { type: string | undefined; uri: string | undefined }[] | undefined;
+      video: { type: string | undefined; uri: string | undefined }[] | undefined;
+      user: { _id: number; name: string; avatar: string } | undefined;
     }[] = [];
 
     if (currentUserId && !!data?.results?.length && !!dataRecipients) {
       data?.results?.forEach((message) => {
         const {
-          id,
+          id: _id,
           user_id,
           content,
-          created_at,
+          created_at: createdAt,
           created_by: createdBy,
-          updated_at
+          updated_at: updatedAt
         } = message || {};
 
-        const user = dataRecipients.find(
-          (recipient: { id: string }) => recipient.id == createdBy.toString()
-        );
-        const { display_name: displayName, guid } = user || {};
-        // get initials from the display name
-        const title = displayName
-          ?.split(' ')
-          .map((part: string) => part[0])
-          .join('');
-        const uri = volunteerProfileImage(guid);
+        const { title, uri } = userInfo(dataRecipients, createdBy) || undefined;
+        const { image, pdf, video } = mediaParser(content) || undefined;
+        const text = textParser(content);
 
         messageArray.push({
-          text: content,
-          createdAt: created_at,
-          updatedAt: updated_at,
-          _id: id,
+          text,
+          createdAt,
+          updatedAt,
+          _id,
+          image,
+          pdf,
+          video,
           user: {
             _id: user_id,
             name: title,
@@ -110,4 +114,55 @@ export const VolunteerMessage = ({
       userId={currentUserId}
     />
   );
+};
+
+// a function to sort different media types and then create a link to the required media file
+const mediaParser = (content: string) => {
+  const dataType = content
+    ?.split('](file-guid:')
+    ?.map(
+      (type) =>
+        (type?.match(VIDEO_TYPE_REGEX) ||
+          type?.match(IMAGE_TYPE_REGEX) ||
+          type?.match(PDF_TYPE_REGEX))?.[0]
+    )
+    ?.filter((type) => type);
+
+  const urisWithDataType = content?.match(GUID_REGEX)?.map((dataGUID, index) => ({
+    type: dataType?.[index],
+    uri: volunteerMessageMedia(dataGUID)
+  }));
+
+  return {
+    image: urisWithDataType?.filter(({ type }) => type?.match(IMAGE_TYPE_REGEX)),
+    pdf: urisWithDataType?.filter(({ type }) => type?.match(PDF_TYPE_REGEX)),
+    video: urisWithDataType?.filter(({ type }) => type?.match(VIDEO_TYPE_REGEX))
+  };
+};
+
+// if there is text in the content with the image, the text is only after `)`
+//   so we need to separate it to get the text
+// to access the text message, we need to extract the ones that do not have GUID
+//   information in the array
+const textParser = (content: string) =>
+  content
+    ?.split(')')
+    ?.map((text) => !text?.match(GUID_REGEX) && text)
+    ?.filter((text) => text)
+    ?.toString();
+
+// function used to generate user information
+const userInfo = (dataRecipients: any[], createdBy: number) => {
+  const user = dataRecipients.find(
+    (recipient: { id: string }) => recipient.id == createdBy.toString()
+  );
+  const { display_name: displayName, guid } = user || {};
+  // get initials from the display name
+  const title = displayName
+    ?.split(' ')
+    .map((part: string) => part[0])
+    .join('');
+  const uri = volunteerProfileImage(guid);
+
+  return { title, uri };
 };
