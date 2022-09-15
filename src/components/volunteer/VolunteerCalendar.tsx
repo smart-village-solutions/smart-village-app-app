5a7f8bca051b8ba6dcdd0e902a244e071fe8e927
Daniel Molnar <dm@tpwd.de>
2022-09-15 16:18:13 +0200

    fix(volunteer): remove android margin above calendar

--- src/components/volunteer/VolunteerCalendar.tsx
+++ src/components/volunteer/VolunteerCalendar.tsx
@@ -1,13 +1,11 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import _isNumber from 'lodash/isNumber';
 import moment from 'moment';
 import 'moment/locale/de';
 import React from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
 import { Calendar, CalendarProps } from 'react-native-calendars';
 import BasicDay, { BasicDayProps } from 'react-native-calendars/src/calendar/day/basic';
 
-import { colors, consts, normalize, texts } from '../../config';
+import { colors, consts, texts } from '../../config';
 import { setupLocales } from '../../helpers';
 import { ScreenName, VolunteerCalendar as TVolunteerCalendar } from '../../types';
 import { renderArrow } from '../calendarArrows';
@@ -72,46 +70,31 @@ export const VolunteerCalendar = ({
   const contentContainerId = queryVariables?.contentContainerId;
 
   return (
-    <View style={styles.topMarginContainer}>
-      <Calendar
-        dayComponent={DayComponent}
-        onDayPress={(day) =>
-          navigation.push(ScreenName.VolunteerIndex, {
-            title: texts.volunteer.calendar,
-            query,
-            queryVariables: { dateRange: [day.dateString], contentContainerId },
-            rootRouteName: ROOT_ROUTE_NAMES.VOLUNTEER
-          })
+    <Calendar
+      dayComponent={DayComponent}
+      onDayPress={(day) =>
+        navigation.push(ScreenName.VolunteerIndex, {
+          title: texts.volunteer.calendar,
+          query,
+          queryVariables: { dateRange: [day.dateString], contentContainerId },
+          rootRouteName: ROOT_ROUTE_NAMES.VOLUNTEER
+        })
+      }
+      displayLoadingIndicator={isLoading}
+      markedDates={getMarkedDates(calendarData)}
+      markingType="multi-dot"
+      renderArrow={renderArrow}
+      firstDay={1}
+      theme={{
+        todayTextColor: colors.primary,
+        indicatorColor: colors.primary,
+        dotStyle: {
+          borderRadius: DOT_SIZE / 2,
+          height: DOT_SIZE,
+          width: DOT_SIZE
         }
-        displayLoadingIndicator={isLoading}
-        markedDates={getMarkedDates(calendarData)}
-        markingType="multi-dot"
-        renderArrow={renderArrow}
-        firstDay={1}
-        theme={{
-          todayTextColor: colors.primary,
-          indicatorColor: colors.primary,
-          dotStyle: {
-            borderRadius: DOT_SIZE / 2,
-            height: DOT_SIZE,
-            width: DOT_SIZE
-          }
-        }}
-        enableSwipeMonths
-      />
-      {/* TODO: show dot with color for the different calendars available */}
-      {/* <WasteCalendarLegend data={usedTypes} /> */}
-    </View>
+      }}
+      enableSwipeMonths
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  topMarginContainer: {
-    ...Platform.select({
-      android: {
-        marginTop: normalize(44)
-      },
-      ios: {}
-    })
-  }
-});
