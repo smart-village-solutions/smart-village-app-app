8191a748e97f214cda22362df2e1170ff7cc434c
Arda Senturk <ase@tpwd.de>
2022-10-05 12:33:54 +0200

    feat: add image to service page
    - added `imageKey` to `defaultStackNavigator`
      for the key of the image information added to
      the server
    - added image link as a prop to `ServiceTiles`
    - checking the image information and displaying
      it on the screen if there is an image
    
    SVA-761

--- src/screens/TilesScreen.js
+++ src/screens/TilesScreen.js
@@ -5,15 +5,28 @@ import { ServiceTiles } from '../components';
 import { useMatomoTrackScreenView } from '../hooks';
 import { SettingsContext } from '../SettingsProvider';
 
-export const getTilesScreen = ({ matomoString, staticJsonName, titleFallback, titleKey }) => {
+export const getTilesScreen = ({
+  matomoString,
+  staticJsonName,
+  titleFallback,
+  titleKey,
+  imageKey
+}) => {
   const TilesScreen = ({ navigation }) => {
     const { globalSettings } = useContext(SettingsContext);
     const { sections = {} } = globalSettings;
-    const { [titleKey]: title = titleFallback } = sections;
+    const { [titleKey]: title = titleFallback, [imageKey]: image } = sections;
 
     useMatomoTrackScreenView(matomoString);
 
-    return <ServiceTiles navigation={navigation} staticJsonName={staticJsonName} title={title} />;
+    return (
+      <ServiceTiles
+        navigation={navigation}
+        staticJsonName={staticJsonName}
+        title={title}
+        image={image}
+      />
+    );
   };
 
   TilesScreen.propTypes = {
