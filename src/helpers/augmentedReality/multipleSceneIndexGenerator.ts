0956602b0a4206c45f38336bca153aaeef135ab4
Arda Senturk <ase@tpwd.de>
2022-11-29 17:31:01 +0100

    refactor(ar): replace async with void function
    - changed the `multipleSceneIndexGenerator` function
      to `void` format as there is no need for `async/await`
      function
    
    SVA-565

--- src/helpers/augmentedReality/multipleSceneIndexGenerator.ts
+++ src/helpers/augmentedReality/multipleSceneIndexGenerator.ts
@@ -3,6 +3,9 @@ import { extendMoment } from 'moment-range';
 
 const extendedMoment = extendMoment(moment);
 
+type TGenerator = { startDate: Date; timePeriodInDays: number; scenes: Array<TScenes> };
+type TScenes = { localUris: Array<{ type: string; stable: boolean }> };
+
 /**
  * index creation function for model and texture
  *
@@ -12,25 +15,28 @@ const extendedMoment = extendMoment(moment);
  *
  * @return {object} both parsed values as an object, like { modalIndex: 1, textureIndex: 1 }
  */
-const multipleSceneIndexGenerator = async ({ startDate, timePeriodInDays, scenes }) =>
-  new Promise((resolve) => {
-    let modalIndex, textureIndex;
+export const multipleSceneIndexGenerator = ({
+  startDate,
+  timePeriodInDays,
+  scenes
+}: TGenerator) => {
+  let modalIndex, textureIndex;
 
-    /* all models must have the same number of variable textures. Therefore, in order to obtain the 
-       number of textures, the textures of the first model were calculated. */
-    if (scenes.length > 1) {
-      const today = new Date(),
-        differenceDate = extendedMoment.range(startDate, today).diff('days'),
-        textureCount = scenes[0]?.localUris?.filter(
-          ({ type, stable }) => !stable && type === 'texture'
-        )?.length,
-        texture = Math.floor(differenceDate / timePeriodInDays),
-        modalCount = scenes.length,
-        modal = Math.floor(texture / textureCount);
+  /* all models must have the same number of variable textures. Therefore, in order to obtain the 
+      number of textures, the textures of the first model were calculated. */
+  if (scenes.length > 1) {
+    const today = new Date(),
+      differenceDate = extendedMoment.range(startDate, today).diff('days'),
+      textureCount = scenes[0]?.localUris?.filter(
+        ({ type, stable }) => !stable && type === 'texture'
+      )?.length,
+      texture = Math.floor(differenceDate / timePeriodInDays),
+      modalCount = scenes.length,
+      modal = Math.floor(texture / textureCount);
 
-      modalIndex = modal % modalCount;
-      textureIndex = texture % textureCount;
-    }
+    modalIndex = modal % modalCount;
+    textureIndex = texture % textureCount;
+  }
 
-    resolve({ modalIndex, textureIndex });
-  });
+  return { modalIndex, textureIndex };
+};
