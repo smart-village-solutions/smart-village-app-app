407673b2fe5208da10bd36f9bf6499fc2cf50ea6
Arda Senturk <ase@tpwd.de>
2022-11-30 16:18:17 +0100

    refactor(ar): delete variable textures from `localUris`
    - deleted variable textures to keep other data in
      `localUris` and add variable textures, then added
      `variableTexture` selected according to `textureIndex`
    - returned the `localUris` object without performing
      mathematical operations if `variableTexturesCount` is 0
    - updated names to improve clarity
    
    SVA-565

--- src/helpers/augmentedReality/multipleSceneIndexGenerator.ts
+++ src/helpers/augmentedReality/multipleSceneIndexGenerator.ts
@@ -1,10 +1,11 @@
+import _remove from 'lodash/remove';
 import moment from 'moment';
 import { extendMoment } from 'moment-range';
 
 const extendedMoment = extendMoment(moment);
 
 type TScenes = { localUris: Array<{ type: string; stable: boolean }> };
-type TGenerator = { startDate: Date; timePeriodInDays: number; scenes: Array<TScenes> };
+type TGenerator = { startDate?: Date; timePeriodInDays?: number; scenes: Array<TScenes> };
 
 /**
  * index creation function for model and texture
@@ -22,27 +23,36 @@ export const multipleSceneIndexGenerator = ({
 }: TGenerator) => {
   let modelIndex = 0;
   const scenesCount = scenes?.length;
-  let localUris = scenes?.[modelIndex]?.localUris;
+  const localUris = scenes?.[modelIndex]?.localUris;
 
   // if we have multiple scenes, we want to calculate the model and texture based on the current day
   // compared to the `startDate`
   if (scenesCount > 1 && startDate && timePeriodInDays) {
-    const today = new Date();
-
-    // the current day number is the number of running days since the given start date
-    const currentDayNumber = extendedMoment.range(startDate, today).diff('days');
-
     // all models must have the same number of variable textures. therefore, in order to obtain the
     // number of textures, the textures of the first model were calculated.
-    const textureCount = scenes[0]?.localUris?.filter(
-      ({ type, stable }) => !stable && type === 'texture'
-    )?.length;
+    const variableTextures = scenes[0]?.localUris?.filter(
+      ({ type, stable }) => type === 'texture' && !stable
+    );
+    const variableTexturesCount = variableTextures?.length;
 
+    if (!variableTexturesCount) {
+      // nothing to do if there are no unstable textures
+      // keep original localUris with modelIndex = 0
+      return { localUris };
+    }
+
+    const today = new Date();
+    // the current day number is the number of running days since the given start date
+    const currentDayNumber = extendedMoment.range(startDate, today).diff('days');
     const texture = Math.floor(currentDayNumber / timePeriodInDays);
-    const model = Math.floor(texture / textureCount);
+    const model = Math.floor(texture / variableTexturesCount);
     modelIndex = model % scenesCount;
-    const textureIndex = texture % textureCount;
-    localUris = [scenes[modelIndex]?.localUris[textureIndex]];
+    const textureIndex = texture % variableTexturesCount;
+    const variableTexture = variableTextures?.[textureIndex];
+
+    _remove(localUris, ({ type, stable }) => type === 'texture' && !stable);
+
+    !!variableTexture && localUris.push(variableTexture);
   }
 
   return { localUris };
