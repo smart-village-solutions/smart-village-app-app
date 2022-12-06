c6b039c1c3361edf0faeceff58ca85b211db70be
Arda Senturk <ase@tpwd.de>
2022-12-06 14:11:22 +0100

    feat(ar): add array for models and textures
    - added multiple models and textures from the `scene`
      array to the `models` and `textures` arrays in order
      to show the models using the map function on the
      `AugmentedRealityView` screen
    - deleted objects according to their types with `_remove`
      method to prevent unnecessary texture and model information
    - creating `variableTextures` based on index 0 of the
      scene and we were constantly getting variable textures
      of the model at index 0. To avoid this, `variableTextures`
      has been updated with `modelIndex`
    - deleted the direct return of `localUris` in case
      `variableTexturesCount` is `false` because we need
      `models` and `textures` arrays inside `localUris`
    
    SVA-565

--- src/helpers/augmentedReality/multipleSceneIndexGenerator.ts
+++ src/helpers/augmentedReality/multipleSceneIndexGenerator.ts
@@ -4,7 +4,25 @@ import { extendMoment } from 'moment-range';
 
 const extendedMoment = extendMoment(moment);
 
-type TScenes = { localUris: Array<{ type: string; stable: boolean }> };
+type TTextures = Array<{ size: number; stable: boolean; title: string; type: string; uri: string }>;
+type TModels = Array<{
+  position?: number[];
+  rotation?: number[];
+  scale?: number[];
+  size?: number;
+  title?: string;
+  type: string;
+  uri: string;
+}>;
+type TScenes = {
+  localUris: Array<{
+    models?: TModels;
+    stable?: boolean;
+    textures?: TTextures;
+    type?: string;
+    uri?: string;
+  }>;
+};
 type TGenerator = { startDate?: Date; timePeriodInDays?: number; scenes: Array<TScenes> };
 
 /**
@@ -24,22 +42,18 @@ export const multipleSceneIndexGenerator = ({
   let modelIndex = 0;
   const scenesCount = scenes?.length;
   let localUris = scenes?.[modelIndex]?.localUris;
+  const models: TModels = [];
+  const textures: TTextures = [];
 
   // if we have multiple scenes, we want to calculate the model and texture based on the current day
   // compared to the `startDate`
   if (scenesCount > 1 && startDate && timePeriodInDays) {
     // all models must have the same number of variable textures. therefore, in order to obtain the
     // number of textures, the textures of the first model were calculated.
-    const variableTextures = scenes[0]?.localUris?.filter(
+    let variableTextures = scenes[0]?.localUris?.filter(
       ({ type, stable }) => type === 'texture' && !stable
     );
     const variableTexturesCount = variableTextures?.length;
-
-    if (!variableTexturesCount) {
-      // nothing to do if there are no unstable textures
-      // keep original localUris with modelIndex = 0
-      return { localUris };
-    }
 
     const today = new Date();
     // the current day number is the number of running days since the given start date
@@ -47,13 +61,54 @@ export const multipleSceneIndexGenerator = ({
     const texture = Math.floor(currentDayNumber / timePeriodInDays);
     const model = Math.floor(texture / variableTexturesCount);
     modelIndex = model % scenesCount;
-    const textureIndex = texture % variableTexturesCount;
-    const variableTexture = variableTextures?.[textureIndex];
+
+    // When VariableTextures are created, they are created according to index 0 of the scene array.
+    // Once the modelIndex is found, it must be updated to select the correct texture file.
+    variableTextures = scenes[modelIndex]?.localUris?.filter(
+      ({ type, stable }) => type === 'texture' && !stable
+    );
     localUris = scenes?.[modelIndex]?.localUris;
+    const textureIndex = texture % variableTexturesCount;
+    const variableTexture = variableTextures[textureIndex];
 
-    _remove(localUris, ({ type, stable }) => type === 'texture' && !stable);
+    localUris.forEach((item) => {
+      switch (item.type) {
+        case 'vrx':
+          models.push(item as TModels[0]);
+          break;
+        case 'texture':
+          if (item.stable) {
+            textures.push(item as TTextures[0]);
+            !!variableTexture && textures.push(variableTexture as TTextures[0]);
+          }
+          break;
+        default:
+          break;
+      }
+    });
 
-    !!variableTexture && localUris.push(variableTexture);
+    _remove(localUris, ({ type }) => type === 'texture' || type === 'vrx');
+
+    localUris.push({ models, textures });
+  } else {
+    scenes.forEach(({ localUris: multiModelLocalUris }) => {
+      multiModelLocalUris.forEach((item) => {
+        switch (item.type) {
+          case 'vrx':
+            models.push(item as TModels[0]);
+            break;
+          case 'texture':
+            textures.push(item as TTextures[0]);
+            break;
+          default:
+            break;
+        }
+      });
+    });
+
+    _remove(localUris, ({ type }) => type === 'texture' || type === 'vrx');
+
+    localUris.push({ models, textures });
   }
 
   return { localUris };
