2281e1621d5611c28723447867695f7e62cb384c
Daniel <dm@tpwd.de>
2023-08-18 15:26:07 +0200

    feat: position of floating button depending on navigation
    - the bottom position needs to depend on the navigation
      "drawer" or "tab" to be placed correctly
    
    MQGB-34

--- src/components/IndexMapSwitch.js
+++ src/components/IndexMapSwitch.js
@@ -1,45 +1,61 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { texts } from '../config';
+import { SettingsContext } from '../SettingsProvider';
 
 import { Button } from './Button';
 
-export const IndexMapSwitch = ({ filter, setFilter }) => (
-  <View style={styles.floatingButtonContainer}>
-    <Button
-      onPress={() => {
-        const selectedFilter = filter.find((entry) => entry.selected);
-        const updatedFilter = filter.map((entry) => {
-          if (entry.id !== selectedFilter.id) {
-            entry.selected = true;
-          } else {
-            entry.selected = false;
-          }
+export const IndexMapSwitch = ({ filter, setFilter }) => {
+  const { globalSettings } = useContext(SettingsContext);
+  const { navigation = 'drawer' } = globalSettings;
 
-          return entry;
-        });
+  return (
+    <View style={[styles.floatingButtonContainer, stylesWithProps({ navigation }).position]}>
+      <Button
+        onPress={() => {
+          const selectedFilter = filter.find((entry) => entry.selected);
+          const updatedFilter = filter.map((entry) => {
+            if (entry.id !== selectedFilter.id) {
+              entry.selected = true;
+            } else {
+              entry.selected = false;
+            }
 
-        setFilter(updatedFilter);
-      }}
-      title={
-        filter.find((entry) => entry.selected).title === texts.locationOverview.list
-          ? texts.locationOverview.map
-          : texts.locationOverview.list
-      }
-      notFullWidth
-    />
-  </View>
-);
+            return entry;
+          });
+
+          setFilter(updatedFilter);
+        }}
+        title={
+          filter.find((entry) => entry.selected).title === texts.locationOverview.list
+            ? texts.locationOverview.map
+            : texts.locationOverview.list
+        }
+        notFullWidth
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   floatingButtonContainer: {
     alignSelf: 'center',
-    bottom: '5%',
     position: 'absolute'
   }
 });
+
+/* eslint-disable react-native/no-unused-styles */
+/* this works properly, we do not want that warning */
+const stylesWithProps = ({ navigation }) => {
+  return StyleSheet.create({
+    position: {
+      bottom: navigation === 'tab' ? 0 : '5%'
+    }
+  });
+};
+/* eslint-enable react-native/no-unused-styles */
 
 IndexMapSwitch.propTypes = {
   filter: PropTypes.array.isRequired,
