b3c36e0abef1a2dc2e5af6abe0217a19c17ba447
Arda Sentürk <ase@tpwd.de>
2023-09-26 09:12:44 +0200

    feat: add options to customise `TextListItem`
    - added new style props to customise the `TextListItem` via props
    - created `textDirection` depending on the `titleFirst´ prop to change the order of the texts
    
    MQGB-34

--- src/components/TextListItem.tsx
+++ src/components/TextListItem.tsx
@@ -1,16 +1,17 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import PropTypes from 'prop-types';
 import React, { memo, NamedExoticComponent, Validator } from 'react';
-import { StyleSheet } from 'react-native';
+import { ImageStyle, StyleSheet, ViewStyle } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
 import { colors, consts, Icon, normalize } from '../config';
-import { trimNewLines } from '../helpers';
+import { isOpen, trimNewLines } from '../helpers';
 
 import { Image } from './Image';
 import { BoldText, RegularText } from './Text';
 import { Touchable } from './Touchable';
 import { WrapperRow } from './Wrapper';
+import { OpenStatus } from './infoCard/OpenStatus';
 
 export type ItemData = {
   id: string;
@@ -30,10 +31,14 @@ export type ItemData = {
 };
 
 type Props = {
+  containerStyle?: ViewStyle;
+  imageContainerStyle?: ViewStyle;
+  imageStyle?: ImageStyle;
   item: ItemData;
   leftImage?: boolean | undefined;
   navigation: StackNavigationProp<Record<string, any>>;
   noSubtitle?: boolean | undefined;
+  titleFirst?: boolean;
 };
 
 /* eslint-disable complexity */
@@ -41,80 +46,96 @@ export const TextListItem: NamedExoticComponent<Props> & {
   propTypes?: Record<string, Validator<any>>;
 } & {
   defaultProps?: Partial<Props>;
-} = memo<Props>(({ item, leftImage, navigation, noSubtitle }) => {
-  const {
-    badge,
-    bottomDivider,
-    leftIcon,
-    onPress,
-    params,
-    picture,
-    routeName: name,
-    statustitle,
-    statustitleIcon,
-    subtitle,
-    teaserTitle,
-    title,
-    topDivider
-  } = item;
-  const navigate = () => navigation && navigation.push(name, params);
-  let titleText = <BoldText>{trimNewLines(title)}</BoldText>;
+} = memo<Props>(
+  ({
+    containerStyle,
+    imageContainerStyle,
+    imageStyle,
+    item,
+    leftImage,
+    navigation,
+    noSubtitle,
+    titleFirst
+  }) => {
+    const {
+      badge,
+      bottomDivider,
+      leftIcon,
+      onPress,
+      params,
+      picture,
+      routeName: name,
+      statustitle,
+      statustitleIcon,
+      subtitle,
+      teaserTitle,
+      title,
+      topDivider
+    } = item;
+    const navigate = () => navigation && navigation.push(name, params);
+    let titleText = <BoldText>{trimNewLines(title)}</BoldText>;
 
-  if (teaserTitle) {
-    titleText = (
-      <>
-        {titleText}
-        <RegularText small>{teaserTitle}</RegularText>
-      </>
+    const textDirection: 'column' | 'column-reverse' = titleFirst ? 'column-reverse' : 'column';
+
+    if (teaserTitle) {
+      titleText = (
+        <>
+          {titleText}
+          <RegularText small>{teaserTitle}</RegularText>
+        </>
+      );
+    }
+
+    if (statustitle) {
+      titleText = (
+        <>
+          {titleText}
+          <WrapperRow style={styles.statustitleWrapper}>
+            {!!statustitleIcon && statustitleIcon}
+            <RegularText small placeholder>
+              {statustitle}
+            </RegularText>
+          </WrapperRow>
+        </>
+      );
+    }
+
+    // `title` is the first line and `subtitle` the second line, so `title` is used with our subtitle
+    // content and `subtitle` is used with the main title
+    return (
+      <ListItem
+        bottomDivider={bottomDivider !== undefined ? bottomDivider : true}
+        topDivider={topDivider !== undefined ? topDivider : false}
+        containerStyle={[styles.container, containerStyle && containerStyle]}
+        badge={badge}
+        onPress={() => (onPress ? onPress(navigation) : navigate())}
+        disabled={!navigation}
+        delayPressIn={0}
+        Component={Touchable}
+        accessibilityLabel={`(${title}) ${consts.a11yLabel.button}`}
+      >
+        {leftIcon ||
+          (leftImage && !!picture?.url ? (
+            <Image
+              source={{ uri: picture.url }}
+              style={[styles.smallImage, imageStyle && imageStyle]}
+              containerStyle={[
+                styles.smallImageContainer,
+                imageContainerStyle && imageContainerStyle
+              ]}
+            />
+          ) : undefined)}
+
+        <ListItem.Content style={{ flexDirection: textDirection }}>
+          {noSubtitle || !subtitle ? titleText : <RegularText small>{subtitle}</RegularText>}
+          {noSubtitle || !subtitle ? undefined : titleText}
+        </ListItem.Content>
+
+        {!!navigation && <Icon.ArrowRight color={colors.darkText} size={normalize(18)} />}
+      </ListItem>
     );
   }
-
-  if (statustitle) {
-    titleText = (
-      <>
-        {titleText}
-        <WrapperRow style={styles.statustitleWrapper}>
-          {!!statustitleIcon && statustitleIcon}
-          <RegularText small placeholder>
-            {statustitle}
-          </RegularText>
-        </WrapperRow>
-      </>
-    );
-  }
-
-  // `title` is the first line and `subtitle` the second line, so `title` is used with our subtitle
-  // content and `subtitle` is used with the main title
-  return (
-    <ListItem
-      bottomDivider={bottomDivider !== undefined ? bottomDivider : true}
-      topDivider={topDivider !== undefined ? topDivider : false}
-      containerStyle={styles.container}
-      badge={badge}
-      onPress={() => (onPress ? onPress(navigation) : navigate())}
-      disabled={!navigation}
-      delayPressIn={0}
-      Component={Touchable}
-      accessibilityLabel={`(${title}) ${consts.a11yLabel.button}`}
-    >
-      {leftIcon ||
-        (leftImage && !!picture?.url ? (
-          <Image
-            source={{ uri: picture.url }}
-            style={styles.smallImage}
-            containerStyle={styles.smallImageContainer}
-          />
-        ) : undefined)}
-
-      <ListItem.Content>
-        {noSubtitle || !subtitle ? titleText : <RegularText small>{subtitle}</RegularText>}
-        {noSubtitle || !subtitle ? undefined : titleText}
-      </ListItem.Content>
-
-      {!!navigation && <Icon.ArrowRight color={colors.darkText} size={normalize(18)} />}
-    </ListItem>
-  );
-});
+);
 /* eslint-enable complexity */
 
 const styles = StyleSheet.create({
@@ -137,10 +158,14 @@ const styles = StyleSheet.create({
 TextListItem.displayName = 'TextListItem';
 
 TextListItem.propTypes = {
+  containerStyle: PropTypes.object,
+  imageContainerStyle: PropTypes.object,
+  imageStyle: PropTypes.object,
   item: PropTypes.object.isRequired,
   leftImage: PropTypes.bool,
   navigation: PropTypes.object,
-  noSubtitle: PropTypes.bool
+  noSubtitle: PropTypes.bool,
+  titleFirst: PropTypes.bool
 };
 
 TextListItem.defaultProps = {
