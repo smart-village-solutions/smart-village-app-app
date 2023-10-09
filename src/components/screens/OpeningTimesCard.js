6492953581ed702887402e4514ea41ba5e0d4f64
Arda Sentürk <ase@tpwd.de>
2023-10-09 11:13:45 +0200

    feat: show appointments from today
    - added filter to `openingHours` array to show date information
      in the appointment list from today
    
    SVA-1176

--- src/components/screens/OpeningTimesCard.js
+++ src/components/screens/OpeningTimesCard.js
@@ -4,7 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import styled from 'styled-components/native';
 
 import { colors, normalize } from '../../config';
-import { momentFormat } from '../../helpers';
+import { isTodayOrLater, momentFormat } from '../../helpers';
 import { HtmlView } from '../HtmlView';
 import { BoldText, RegularText } from '../Text';
 import { Wrapper, WrapperRow } from '../Wrapper';
@@ -26,65 +26,71 @@ const DateBox = styled(TimeBox)`
 export const OpeningTimesCard = ({ openingHours }) => (
   <Wrapper>
     {!!openingHours &&
-      openingHours.map((item, index) => {
-        const {
-          weekday,
-          timeFrom,
-          timeTo,
-          dateFrom,
-          dateTo,
-          description,
-          open,
-          useYear = false
-        } = item;
-        const returnFormatDate = useYear ? 'DD.MM.YYYY' : 'DD.MM.';
+      openingHours
+        .filter((item) => {
+          const { dateFrom, dateTo } = item;
 
-        return (
-          <View key={index} style={index !== openingHours.length - 1 ? styles.divider : null}>
-            {!!weekday && <BoldText style={styles.marginBottom}>{weekday}</BoldText>}
+          return isTodayOrLater(dateTo || dateFrom);
+        })
+        .map((item, index) => {
+          const {
+            weekday,
+            timeFrom,
+            timeTo,
+            dateFrom,
+            dateTo,
+            description,
+            open,
+            useYear = false
+          } = item;
+          const returnFormatDate = useYear ? 'DD.MM.YYYY' : 'DD.MM.';
 
-            {(!!timeFrom || !!timeTo || !!dateFrom || !!dateTo) && (
-              <WrapperRow>
-                {(open === undefined || open === true) && (!!timeFrom || !!timeTo) && (
-                  <TimeBox>
-                    {!!timeFrom && <RegularText>{timeFrom}</RegularText>}
-                    {!!timeFrom && !!timeTo && <RegularText> -</RegularText>}
-                    {!!timeTo && <RegularText> {timeTo}</RegularText>}
-                  </TimeBox>
-                )}
-                {open === false && (
-                  <TimeBox>
-                    <RegularText>geschlossen</RegularText>
-                  </TimeBox>
-                )}
-                {(!!dateFrom || !!dateTo) && (
-                  <DateBox>
-                    {!!dateFrom && (
-                      <RegularText>
-                        <RegularText small />
-                        {momentFormat(dateFrom, returnFormatDate)}
-                      </RegularText>
-                    )}
+          return (
+            <View key={index} style={index !== openingHours.length - 1 ? styles.divider : null}>
+              {!!weekday && <BoldText style={styles.marginBottom}>{weekday}</BoldText>}
 
-                    {!!dateTo && dateTo !== dateFrom && (
-                      <RegularText>
-                        <RegularText small>bis </RegularText>
-                        {momentFormat(dateTo, returnFormatDate)}
-                      </RegularText>
-                    )}
-                  </DateBox>
-                )}
-              </WrapperRow>
-            )}
+              {(!!timeFrom || !!timeTo || !!dateFrom || !!dateTo) && (
+                <WrapperRow>
+                  {(open === undefined || open === true) && (!!timeFrom || !!timeTo) && (
+                    <TimeBox>
+                      {!!timeFrom && <RegularText>{timeFrom}</RegularText>}
+                      {!!timeFrom && !!timeTo && <RegularText> -</RegularText>}
+                      {!!timeTo && <RegularText> {timeTo}</RegularText>}
+                    </TimeBox>
+                  )}
+                  {open === false && (
+                    <TimeBox>
+                      <RegularText>geschlossen</RegularText>
+                    </TimeBox>
+                  )}
+                  {(!!dateFrom || !!dateTo) && (
+                    <DateBox>
+                      {!!dateFrom && (
+                        <RegularText>
+                          <RegularText small />
+                          {momentFormat(dateFrom, returnFormatDate)}
+                        </RegularText>
+                      )}
 
-            {!!description && (
-              <WrapperRow>
-                <HtmlView html={description} />
-              </WrapperRow>
-            )}
-          </View>
-        );
-      })}
+                      {!!dateTo && dateTo !== dateFrom && (
+                        <RegularText>
+                          <RegularText small>bis </RegularText>
+                          {momentFormat(dateTo, returnFormatDate)}
+                        </RegularText>
+                      )}
+                    </DateBox>
+                  )}
+                </WrapperRow>
+              )}
+
+              {!!description && (
+                <WrapperRow>
+                  <HtmlView html={description} />
+                </WrapperRow>
+              )}
+            </View>
+          );
+        })}
   </Wrapper>
 );
 /* eslint-enable complexity */
