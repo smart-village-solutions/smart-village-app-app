6934572d8c23eac3357943598fb6d47c79af2bcc
Arda Sentürk <ase@tpwd.de>
2023-10-26 16:51:41 +0200

    feat(sue): add address component
    - created `SUEAddress` component to be visible in list view
    
    SUE-5

---
+++ src/components/SUE/SUEAddress.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { RegularText } from '../Text';
+import { Wrapper } from '../Wrapper';
+
+export const SUEAddress = ({ address }: { address: string }) => {
+  return (
+    <Wrapper style={styles.noPadding}>
+      <RegularText small>{address}</RegularText>
+    </Wrapper>
+  );
+};
+
+const styles = StyleSheet.create({
+  noPadding: {
+    paddingBottom: 0
+  }
+});
--- src/components/SUE/index.ts
+++ src/components/SUE/index.ts
@@ -1,2 +1,3 @@
+export * from './SUEAddress';
 export * from './SueMapView';
 export * from './SUEStatus';
