f8247083b4cd39e9a96edcba4ce4f175362f7eb8
Daniel <dm@tpwd.de>
2023-10-27 09:27:40 +0200

    refactor(sue): reduce logics for detail screen
    - reduced logics to determine component and early return
      sue detail screen if the query matches
    - moved data fetching for sue detail screen in sue detail
      screen
      - added console.warn to present data to work with in the next
        commit
    - added base container rendering for detail screen with
      scroll view
    
    SUE-6

--- src/components/screens/SUE/SueDetailScreen.tsx
+++ src/components/screens/SUE/SueDetailScreen.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { RefreshControl, ScrollView, Text } from 'react-native';
+import { useQuery } from 'react-query';
 
-export const SueDetailScreen = ({ data, navigation, route }) => {
-  return (
-    <View>
-      <Text>DETAIL</Text>
-    </View>
+import { colors } from '../../../config';
+import { QUERY_TYPES, getQuery } from '../../../queries';
+import { SafeAreaViewFlex } from '../../SafeAreaViewFlex';
+
+type Props = {
+  navigation: StackNavigationProp<never>;
+  route: RouteProp<any, never>;
+};
+
+export const SUEDetailScreen = ({ navigation, route }: Props) => {
+  const queryVariables = route.params?.queryVariables ?? {};
+  const [refreshing, setRefreshing] = useState(false);
+
+  const { data, refetch } = useQuery(
+    [QUERY_TYPES.SUE.REQUESTS_WITH_SERVICE_REQUEST_ID, queryVariables?.id],
+    () => getQuery(QUERY_TYPES.SUE.REQUESTS_WITH_SERVICE_REQUEST_ID)(queryVariables?.id)
   );
+
+  const refresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
+  console.warn({ data });
+
+  return (
+    <SafeAreaViewFlex>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={refresh}
+            colors={[colors.refreshControl]}
+            tintColor={colors.refreshControl}
+          />
+        }
+      >
+        <Text>Detail Screen</Text>
+      </ScrollView>
+    </SafeAreaViewFlex>
+  );
+};
+
+SUEDetailScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+  route: PropTypes.object.isRequired
 };
