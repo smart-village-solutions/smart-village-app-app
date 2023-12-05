0598be1ffc98475382223bd691cae418ddf3b846
Arda Sentürk <ase@tpwd.de>
2023-12-05 14:41:14 +0100

    feat: update normalize function
    - updated the normalize function we use to use iPhone 14 Pro (393 x 852) sized devices as the base
      - https://www.ricsantos.net/2021/01/21/ios-device-resolution-guide
    
    MQGB-77

--- src/config/normalize.js
+++ src/config/normalize.js
@@ -35,73 +35,24 @@ const deviceWidth = device.width;
 /* eslint-disable complexity */
 /* this is an external package, we do not lint */
 export const normalize = (size) => {
+  // the base for our calculations are iPhone 14 Pro, iPhone 15, iPhone 15 Pro (393 x 852)
+  // see https://www.ricsantos.net/2021/01/21/ios-device-resolution-guide
+  const ratio = deviceWidth / 393;
+
   if (pixelRatio === 2) {
-    // iphone 5s and older Androids
-    if (deviceWidth < 360) {
-      return size * 0.84;
-    }
-
-    // iphone 5
-    if (deviceHeight < 667) {
-      return size * 0.84;
-
-      // iphone 6-6s
-    } else if (deviceHeight >= 667 && deviceHeight <= 735) {
-      return size;
-    }
-
-    // older phablets
-    return size * 1.12;
+    return size * ratio * 0.84;
   }
 
   if (pixelRatio === 3) {
-    // catch Android font scaling on small machines
-    // where pixel ratio / font scale ratio => 3:3
-    if (deviceWidth <= 360) {
-      return size;
-    }
-
-    // catch other weird android width sizings
-    if (deviceHeight < 667) {
-      return size * 1.12;
-    }
-
-    // catch in-between size Androids and scale font up - a tad but not too much
-    if (deviceHeight >= 667 && deviceHeight <= 735) {
-      return size * 1.12;
-    }
-
-    // iPhone X
-    if (deviceWidth == 375) {
-      return size;
-    }
-
-    // catch larger devices
-    // iphone 6s plus / 7 plus / mi note 等等
-    return size * 1.12;
+    // devices with pixel ratio like 2.6 will round up to 3 (i.e. Google Pixel),
+    // see https://reactnative.dev/docs/next/pixelratio
+    return size * ratio;
   }
 
   if (pixelRatio === 3.5) {
-    // catch Android font scaling on small machines
-    // where pixel ratio / font scale ratio => 3:3
-    if (deviceWidth <= 360) {
-      return size;
-    }
-
-    // catch other smaller android height sizings
-    if (deviceHeight < 667) {
-      return size * 1.18;
-    }
-
-    // catch in-between size Androids and scale font up - a tad but not too much
-    if (deviceHeight >= 667 && deviceHeight <= 735) {
-      return size * 1.18;
-    }
-
-    // catch larger phablet devices
-    return size * 1.2;
+    return size * ratio * 1.18;
   }
 
-  // if older device ie pixelRatio !== 2 || 3 || 3.5
-  return size * 0.84;
+  // if  pixelRatio !== 2 || 3 || 3.5
+  return size * ratio * 0.84;
 };
