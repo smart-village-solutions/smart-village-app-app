4ccb9ac19303620b5181327b17cce3b96cb0a6ec
Arda Sentürk <ase@tpwd.de>
2024-02-29 14:15:58 +0100

    feat: update expo version to sdk 50
    - updated the node version in `.nvmrc` and `eas.json` to 18.19.1 since the lowest node version accepted in the new expo version is 18
    - added `expo-font` and `expo-secure-store` packages to `app.json` as plugins because they need to be added as plugins after the update
    - added `sdkVersion` section to `app.json` because it is necessary when version checking with `npx expo-doctor`
    - reorganised because `metro.config.js` needs to be updated after running `npx expo-doctor`
    - updated required packages
    
    SVA-1305

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { assetExts }
-  } = await getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true
-        }
-      })
-    },
-    resolver: {
-      assetExts: [...assetExts, 'mtl', 'obj', 'vrx']
-    }
-  };
-})();
+config.resolver.sourceExts.push('mtl', 'obj', 'vrx');
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true
+  }
+});
+
+module.exports = config;
