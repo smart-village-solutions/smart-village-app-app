0ce38b527f6f0d9cb9ef301ed4d7deea29c02815
Arda Sentürk <ase@tpwd.de>
2024-05-07 13:59:34 +0200

    feat(voucher): add daily check for account
    - added `accountCheck` function to `VoucherHomeScreen` to check daily if the user's account is still valid with the login credentials saved in the device
    - added `moment` because if the user has not checked the account before, the `savedDate` value will be `null` and the check will not take place
    - saved the date of the check to the device in `accountCheck` after checking the user account, and if the user enters `VoucherHomeScreen` again on the same day,
      the account check is cancelled with the `moment().isSame` function
    - checked whether the user's account is valid or not, if the account is not valid, the `refresh()` function was executed after deleting all user data in `mutateLogin`
      to show the login button on the `VoucherHomeScreen`
    
    SVAK-89

--- src/screens/Voucher/VoucherHomeScreen.tsx
+++ src/screens/Voucher/VoucherHomeScreen.tsx
@@ -1,6 +1,8 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React, { useCallback, useEffect } from 'react';
+import moment from 'moment';
+import React, { useCallback, useEffect, useState } from 'react';
 import { RefreshControl, ScrollView, StyleSheet } from 'react-native';
+import { useMutation } from 'react-query';
 
 import {
   Button,
@@ -12,11 +14,22 @@ import {
   Wrapper
 } from '../../components';
 import { colors, texts } from '../../config';
+import { addToStore, readFromStore } from '../../helpers';
+import {
+  storeVoucherAuthToken,
+  storeVoucherMemberId,
+  storeVoucherMemberLoginInfo,
+  voucherMemberLoginInfo
+} from '../../helpers/voucherHelper';
 import { useStaticContent, useVoucher } from '../../hooks';
-import { ScreenName } from '../../types';
+import { logIn } from '../../queries/vouchers';
+import { ScreenName, VoucherLogin } from '../../types';
+
+const SAVED_DATE = 'savedDate';
 
 export const VoucherHomeScreen = ({ navigation, route }: StackScreenProps<any>) => {
   const { refresh, isLoggedIn, memberId } = useVoucher();
+  const [accountCheckLoading, setAccountCheckLoading] = useState(true);
 
   const imageUri = route?.params?.headerImage;
 
@@ -37,11 +50,40 @@ export const VoucherHomeScreen = ({ navigation, route }: StackScreenProps<any>) 
   // refresh if the refreshAuth param changed, which happens after login
   useEffect(refreshAuth, [route.params?.refreshAuth]);
 
+  const { mutate: mutateLogIn } = useMutation(logIn);
+
+  useEffect(() => {
+    const accountCheck = async () => {
+      const loginData = await voucherMemberLoginInfo();
+      const savedDate = (await readFromStore(SAVED_DATE)) || moment().format('YYYY-MM-DD');
+
+      if (loginData && !moment().isSame(savedDate, 'day')) {
+        mutateLogIn(JSON.parse(loginData as string) as VoucherLogin, {
+          onSuccess: (responseData) => {
+            if (!responseData?.member) {
+              storeVoucherAuthToken();
+              storeVoucherMemberId();
+              storeVoucherMemberLoginInfo();
+              refresh();
+            }
+          }
+        });
+
+        addToStore(SAVED_DATE, moment().format('YYYY-MM-DD'));
+        setAccountCheckLoading(false);
+      } else {
+        setAccountCheckLoading(false);
+      }
+    };
+
+    accountCheck();
+  }, []);
+
   const refreshHome = useCallback(async () => {
     await refetchHomeText();
   }, []);
 
-  if (loadingHomeText) {
+  if (loadingHomeText || accountCheckLoading) {
     return <LoadingSpinner />;
   }
 
