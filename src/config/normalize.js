8a6bc92c65e7f4adad17a232ca237fa5d8890eb7
d4v3d4vE <dwi@tpwd.de>
2024-06-04 09:34:00 +0200

    chore: extend normalize function update
    - added `guidelineBaseHeight` and `PixelRatio` to cover devices with different aspect ratios.
    
    SVA-1351

--- src/config/normalize.js
+++ src/config/normalize.js
@@ -1,12 +1,14 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, PixelRatio } from 'react-native';
 
-// Default guideline sizes are based on standard ~6.12" screen mobile device (iPhone 14 Pro)
 const guidelineBaseWidth = 393;
+const guidelineBaseHeight = 852;
 
 export const normalize = (size, factor = 1) => {
-  const { width, height } = Dimensions.get('window');
-  const shortDimension = width < height ? width : height;
-  const scale = (shortDimension / guidelineBaseWidth) * size;
+   const { width, height } = Dimensions.get('window');
+   const widthScale = width / guidelineBaseWidth;
+   const heightScale = height / guidelineBaseHeight;
+   const scale = Math.min(widthScale, heightScale); // Takes the smaller ratio to avoid excess.
 
-  return size + (scale - size) * factor;
+   const newSize = size * scale;
+   return PixelRatio.roundToNearestPixel(newSize * factor);
 };
