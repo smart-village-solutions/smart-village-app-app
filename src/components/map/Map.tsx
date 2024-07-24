78ae579068e6738e2926138a92d1d6972b2562cd
Arda Sentürk <ardasnturk@gmail.com>
2024-07-24 10:05:36 +0200

    fix(map): app crashes due to `iconName`
    - updated with `undefined` to prevent the app from crashing because the default value was not working due to sending an empty string to iconName in the `MapIcon` component.
    
    MQGB-182

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -264,7 +264,11 @@ export const Map = ({
                       : colors.lighterPrimary
                   }
                   iconName={
-                    marker.iconName ? (isActiveMarker ? 'locationActive' : marker.iconName) : ''
+                    marker.iconName
+                      ? isActiveMarker
+                        ? 'locationActive'
+                        : marker.iconName
+                      : undefined
                   }
                   iconSize={MARKER_ICON_SIZE * (isActiveMarker ? 1.4 : 1.1)}
                 />
