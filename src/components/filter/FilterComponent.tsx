645a908a9a19d740d30d990468345ad6df08d9dd
Daniel <dm@tpwd.de>
2024-10-01 15:29:08 +0200

    fix(filter): layout & paddings
    SVAK-128

--- src/components/filter/FilterComponent.tsx
+++ src/components/filter/FilterComponent.tsx
@@ -4,8 +4,9 @@ import { StyleSheet, View } from 'react-native';
 import { consts, device, normalize } from '../../config';
 import { updateFilters } from '../../helpers';
 import { DatesTypes, DropdownProps, FilterProps, FilterTypesProps, StatusProps } from '../../types';
+import { Label } from '../Label';
 import { Switch } from '../Switch';
-import { BoldText, RegularText } from '../Text';
+import { RegularText } from '../Text';
 
 import { WrapperVertical } from './../Wrapper';
 import { DateFilter } from './DateFilter';
@@ -24,77 +25,79 @@ type Props = {
 export const FilterComponent = ({ filters, filterTypes, setFilters }: Props) => {
   return (
     <>
-      {filterTypes?.map((item) => (
-        <WrapperVertical key={item.name} style={styles.noPaddingBottom}>
-          {item.type === FILTER_TYPES.DATE && (
-            <DateFilter
-              filters={filters}
-              setFilters={setFilters}
-              containerStyle={{ width: device.width * 0.45 }}
-              {...item}
-              data={item.data as DatesTypes[]}
-            />
-          )}
+      {filterTypes
+        ?.filter((item) => !!item.data?.length || item.type === FILTER_TYPES.CHECKBOX)
+        ?.map((item) => (
+          <WrapperVertical key={item.name} style={styles.noPaddingBottom}>
+            {item.type === FILTER_TYPES.DATE && (
+              <DateFilter
+                filters={filters}
+                setFilters={setFilters}
+                containerStyle={{ width: device.width * 0.45 }}
+                {...item}
+                data={item.data as DatesTypes[]}
+              />
+            )}
 
-          {item.type === FILTER_TYPES.DROPDOWN && !!item.data?.length && (
-            <DropdownFilter
-              filters={filters}
-              setFilters={setFilters}
-              {...item}
-              data={item.data as DropdownProps[]}
-              multipleSelect={item.isMultiselect}
-              searchPlaceholder={item.searchPlaceholder}
-              showSearch={item.searchable}
-            />
-          )}
+            {item.type === FILTER_TYPES.DROPDOWN && (
+              <DropdownFilter
+                filters={filters}
+                setFilters={setFilters}
+                {...item}
+                data={item.data as DropdownProps[]}
+                multipleSelect={item.isMultiselect}
+                searchPlaceholder={item.searchPlaceholder}
+                showSearch={item.searchable}
+              />
+            )}
 
-          {item.type === FILTER_TYPES.SUE.STATUS && (
-            <StatusFilter
-              filters={filters}
-              setFilters={setFilters}
-              {...item}
-              data={item.data as StatusProps[]}
-            />
-          )}
+            {item.type === FILTER_TYPES.SUE.STATUS && (
+              <StatusFilter
+                filters={filters}
+                setFilters={setFilters}
+                {...item}
+                data={item.data as StatusProps[]}
+              />
+            )}
 
-          {item.type === FILTER_TYPES.CHECKBOX && (
-            <>
-              <BoldText>{item.label}</BoldText>
-              <View style={styles.checkboxContainerStyle}>
-                <RegularText small>{item.placeholder}</RegularText>
-                <Switch
-                  switchValue={filters[item.name] || false}
-                  toggleSwitch={(value) =>
-                    setFilters(
-                      updateFilters({
-                        currentFilters: filters,
-                        name: item.name,
-                        removeFromFilter: !!item.checked,
-                        value: value
-                      })
-                    )
-                  }
-                />
-              </View>
-            </>
-          )}
+            {item.type === FILTER_TYPES.CHECKBOX && (
+              <>
+                <Label bold>{item.label}</Label>
+                <View style={styles.checkboxContainerStyle}>
+                  <RegularText small>{item.placeholder}</RegularText>
+                  <Switch
+                    switchValue={filters[item.name] || false}
+                    toggleSwitch={(value) =>
+                      setFilters(
+                        updateFilters({
+                          currentFilters: filters,
+                          name: item.name,
+                          removeFromFilter: !!item.checked,
+                          value: value
+                        })
+                      )
+                    }
+                  />
+                </View>
+              </>
+            )}
 
-          {item.type === FILTER_TYPES.SLIDER && (
-            <SliderFilter
-              index={filters?.[item.name]?.index || 0}
-              label={item.label}
-              onSlidingComplete={(index) => {
-                setFilters((prev: FilterProps) => ({
-                  ...prev,
-                  [item.name]: { value: item.data[index], index }
-                }));
-              }}
-              values={item.data as number[]}
-              {...item}
-            />
-          )}
-        </WrapperVertical>
-      ))}
+            {item.type === FILTER_TYPES.SLIDER && (
+              <SliderFilter
+                index={filters?.[item.name]?.index || 0}
+                label={item.label}
+                onSlidingComplete={(index) => {
+                  setFilters((prev: FilterProps) => ({
+                    ...prev,
+                    [item.name]: { value: item.data[index], index }
+                  }));
+                }}
+                values={item.data as number[]}
+                {...item}
+              />
+            )}
+          </WrapperVertical>
+        ))}
     </>
   );
 };
@@ -107,6 +110,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingVertical: normalize(14)
+    paddingBottom: normalize(14),
+    paddingTop: normalize(10)
   }
 });
