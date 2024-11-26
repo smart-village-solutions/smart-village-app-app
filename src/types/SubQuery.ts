3fc21c0f5904d08e7d7221844595adbadccde21b
Arda Sentürk <ardasnturk@gmail.com>
2024-11-26 08:56:26 +0100

    feat: add link that opens from browser
    - added the ability to automatically open the browser if the `WebScreen` in the app is redirected with the `isExternal` param
    - updated `MultiButtonWithSubQuery` to add `isExternal` as param when redirecting via multi button screen
    
    SVASD-15

--- src/types/SubQuery.ts
+++ src/types/SubQuery.ts
@@ -1,6 +1,7 @@
 export type SubQuery = {
   buttons?: [{ buttonTitle: string; routeName: string; webUrl: string }];
   buttonTitle: string;
+  isExternal?: boolean;
   params: { rootRouteName: string; routeName: string; title: string; webUrl: string };
   routeName: string;
   webUrl?: string;
