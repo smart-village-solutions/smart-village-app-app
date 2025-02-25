44464d48e658c5043134f89ba9e499ae6f106a01
Daniel <dm@tpwd.de>
2025-02-25 09:54:18 +0100

    feat(version): show build and ota version
    - added missing build number and version code for platforms in app.json
    - rendered all additional version infos in version number string
    
    GOS-105

--- src/components/VersionNumber.js
+++ src/components/VersionNumber.js
@@ -3,13 +3,15 @@ import { StyleSheet } from 'react-native';
 
 import appJson from '../../app.json';
 import { AccessibilityContext } from '../AccessibilityProvider';
-import { colors, consts, normalize } from '../config';
+import { colors, consts, device, normalize } from '../config';
 
 import { RegularText } from './Text.js';
 import { Wrapper } from './Wrapper.js';
 
 export const VersionNumber = () => {
   const { isReduceTransparencyEnabled } = useContext(AccessibilityContext);
+  const buildNumber =
+    device.platform === 'ios' ? appJson.expo.ios.buildNumber : appJson.expo.android.versionCode;
 
   return (
     <Wrapper>
@@ -18,7 +20,7 @@ export const VersionNumber = () => {
         style={[styles.version, isReduceTransparencyEnabled && styles.accessibilityColor]}
         accessibilityLabel={`${consts.a11yLabel.appVersion} ${appJson.expo.version}`}
       >
-        Version: {appJson.expo.version}
+        Version: {appJson.expo.version} (build: {buildNumber} - OTA: {appJson.expo.otaVersion})
       </RegularText>
     </Wrapper>
   );
