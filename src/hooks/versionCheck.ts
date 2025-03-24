85c458b5f5719cfaf1848671d91ccb7529e5df0d
Arda Sentürk <ardasnturk@gmail.com>
2025-03-24 09:31:32 +0100

    feat(update): integrate in-app updates functionality
    - updated with `expo-in-app-updated` package because `react-native-version-check-expo` package doesn't work on android platform
    - updated `versionCheckEnabled` so that warning texts can be updated from main-server
    - added `isRequired` value to show mandatory update warning
    - added `AppStoreID` and `AppStoreCountry` values to `infoPlist` object in `app.json` to make `expo-in-app-updates` package work for iOS
      - https://github.com/SohelIslamImran/expo-in-app-updates?tab=readme-ov-file#installation
    
    SUE-158
    SVA-1573

--- src/hooks/versionCheck.ts
+++ src/hooks/versionCheck.ts
@@ -1,52 +1,43 @@
+import { checkForUpdate, startUpdate } from 'expo-in-app-updates';
 import { useContext, useEffect } from 'react';
-import { Alert, Linking } from 'react-native';
-import VersionCheck from 'react-native-version-check-expo';
+import { Alert } from 'react-native';
 
-import appJson from '../../app.json';
 import { SettingsContext } from '../SettingsProvider';
-import { device, texts } from '../config';
-
-const bundleIdentifier = appJson.expo.ios.bundleIdentifier;
-const packageName = appJson.expo.android.package;
+import { texts } from '../config';
 
 export const useVersionCheck = () => {
   const { globalSettings } = useContext(SettingsContext);
   const { settings = {} } = globalSettings;
-  const { versionCheckEnabled } = settings;
+  const { versionCheckEnabled = {} } = settings;
+  const {
+    alertMessage = texts.updateAlert.updateMessage,
+    alertTitle = texts.updateAlert.updateTitle,
+    enabled = false,
+    isRequired = false,
+    requiredAlertMessage = texts.updateAlert.updateRequiredMessage,
+    requiredAlertTitle = texts.updateAlert.updateRequiredTitle
+  } = versionCheckEnabled;
 
-  if (!versionCheckEnabled) return;
+  if (!enabled) return;
 
   useEffect(() => {
     const checkAppVersion = async () => {
       try {
-        const storeVersion = await VersionCheck.getLatestVersion({
-          provider: device.platform === 'ios' ? 'appStore' : 'playStore',
-          packageName: device.platform === 'ios' ? bundleIdentifier : packageName,
-          ignoreErrors: true
-        });
+        const { updateAvailable } = await checkForUpdate();
 
-        const currentVersion = await VersionCheck.getCurrentVersion();
+        if (!updateAvailable) return;
 
-        if (!storeVersion || !currentVersion) return;
-
-        if (storeVersion > currentVersion) {
-          const appStoreUrl = await VersionCheck.getAppStoreUrl({ appID: bundleIdentifier });
-          const playStoreUrl = await VersionCheck.getPlayStoreUrl({ appID: packageName });
-
-          Alert.alert(
-            texts.updateAlert.updateRequiredTitle,
-            texts.updateAlert.updateRequiredMessage,
-            [
-              {
-                text: texts.updateAlert.updateNow,
-                onPress: () => {
-                  Linking.openURL(device.platform === 'ios' ? appStoreUrl : playStoreUrl);
-                }
-              }
-            ],
-            { cancelable: false }
-          );
-        }
+        Alert.alert(
+          isRequired ? requiredAlertTitle : alertTitle,
+          isRequired ? requiredAlertMessage : alertMessage,
+          [
+            {
+              text: texts.updateAlert.updateNow,
+              onPress: async () => await startUpdate(isRequired)
+            },
+            !isRequired && { text: texts.updateAlert.later }
+          ]
+        );
       } catch (error) {
         // Handle error while checking app version
         console.error('Error checking app version:', error);
