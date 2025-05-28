35fa27ed2bf7e978a202d91b05fec0fd192f450a
Daniel <dm@tpwd.de>
2025-05-28 11:35:59 +0200

    feat(volunteer): add push token handling in logout process
    - integrate push token mutation in `VolunteerLogoutScreen` at logout acion to delete it from the volunteer instance
    
    EP-50

--- src/queries/volunteer/push.ts
+++ src/queries/volunteer/push.ts
@@ -1,4 +1,4 @@
-import { volunteerApiV2Url } from '../../helpers/volunteerHelper';
+import { volunteerApiV2Url, volunteerAuthToken } from '../../helpers/volunteerHelper';
 import { getPushTokenFromStorage } from '../../pushNotifications';
 
 export const storePushToken = async (authToken: string) => {
@@ -6,6 +6,20 @@ export const storePushToken = async (authToken: string) => {
 
   const fetchObj = {
     method: 'PUT',
+    headers: {
+      Authorization: authToken ? `Bearer ${authToken}` : ''
+    }
+  };
+
+  return await fetch(`${volunteerApiV2Url}sva_push/token/${token}`, fetchObj);
+};
+
+export const deletePushToken = async () => {
+  const authToken = await volunteerAuthToken();
+  const token = await getPushTokenFromStorage();
+
+  const fetchObj = {
+    method: 'DELETE',
     headers: {
       Authorization: authToken ? `Bearer ${authToken}` : ''
     }
