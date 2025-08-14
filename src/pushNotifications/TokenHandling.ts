e07c7067b1b125db0a67a88ee041a48c17af67f8
Daniel <dm@tpwd.de>
2025-08-14 17:33:42 +0200

    feat(push): enhance personalized push settings and toggle functionality
    - updated `SettingsToggle` to conditionally disable interaction based on `isDisabled`
    - refactored `PersonalizedPushSettings` to improve category handling and rendering with section style
    
    SVA-1644

--- src/pushNotifications/TokenHandling.ts
+++ src/pushNotifications/TokenHandling.ts
@@ -141,7 +141,6 @@ export const addExcludeCategoriesPushTokenOnServer = async (
 
     if (!isSuccess) {
       Alert.alert(texts.errors.errorTitle, texts.errors.noData);
-      return false;
     }
 
     return isSuccess;
