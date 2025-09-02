b19063c460fbeaa9d76a096f6b5f8610d0b9be2b
Daniel <dm@tpwd.de>
2025-09-02 10:56:16 +0200

    feat(volunteer): implement comment API
    EPHVL-13

---
+++ src/queries/volunteer/comment.ts
@@ -0,0 +1,103 @@
+import { volunteerApiV1Url, volunteerAuthToken } from '../../helpers/volunteerHelper';
+
+export const commentNew = async ({
+  message,
+  objectId,
+  objectModel
+}: {
+  message: string;
+  objectId: number;
+  objectModel: string;
+}) => {
+  const authToken = await volunteerAuthToken();
+
+  const fetchObj = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: authToken ? `Bearer ${authToken}` : ''
+    },
+    body: JSON.stringify({
+      Comment: {
+        message
+      },
+      objectId,
+      objectModel
+    })
+  };
+
+  return (await fetch(`${volunteerApiV1Url}comment`, fetchObj)).json();
+};
+
+export const comment = async ({ id }: { id: number }) => {
+  const authToken = await volunteerAuthToken();
+
+  const fetchObj = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: authToken ? `Bearer ${authToken}` : ''
+    }
+  };
+
+  return (await fetch(`${volunteerApiV1Url}comment/${id}`, fetchObj)).json();
+};
+
+export const commentEdit = async ({ id, message }: { id: number; message: string }) => {
+  const authToken = await volunteerAuthToken();
+
+  const fetchObj = {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: authToken ? `Bearer ${authToken}` : ''
+    },
+    body: JSON.stringify({
+      Comment: {
+        message
+      }
+    })
+  };
+
+  return (await fetch(`${volunteerApiV1Url}comment/${id}`, fetchObj)).json();
+};
+
+export const commentDelete = async (id: number) => {
+  const authToken = await volunteerAuthToken();
+
+  const fetchObj = {
+    method: 'DELETE',
+    headers: {
+      Authorization: authToken ? `Bearer ${authToken}` : ''
+    }
+  };
+
+  return (await fetch(`${volunteerApiV1Url}comment/${id}`, fetchObj)).json();
+};
+
+export const commentsByObject = async ({
+  objectId,
+  objectModel
+}: {
+  objectId: number;
+  objectModel: string;
+}) => {
+  const authToken = await volunteerAuthToken();
+
+  const fetchObj = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: authToken ? `Bearer ${authToken}` : ''
+    }
+  };
+
+  return (
+    await fetch(
+      `${volunteerApiV1Url}comment/find-by-object?objectModel=${objectModel}&objectId=${objectId}`,
+      fetchObj
+    )
+  ).json();
+};
